8f767b82a2690784ef0cc431fe7bbd17cdac3447
Maldrasen <maldrasen@gmail.com>
2026-02-03 13:14:54 -0600

    Make control start at -200

--- application/fixtures.js
+++ application/fixtures.js
@@ -10,10 +10,9 @@ global.Fixtures = (function() {
     characters.push(CharacterFactory.build({}));
     characters.push(CharacterFactory.build({}));
     characters.forEach(id => {
-      Registry.createControlledComponent(id,{ control:Random.roll(200)-100 });
+      Registry.createControlledComponent(id,{ control:-200 });
       Registry.createSituatedComponent(id,{ currentLocation:'filthy-hovel' });
-
-
+      Registry.createFeelingsComponent(id,{ target:player, affection:-200, respect:0, fear:200 });
     });
 
     StateMachine.setMode(GameMode.location);
