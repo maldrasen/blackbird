8b54f1b9b25625e293c6628dee1c5fb01bb674b2
Maldrasen <maldrasen@gmail.com>
2026-02-10 15:03:44 -0600

    Controllers

---
+++ test/controllers/dungeon-controller-spec.js
@@ -0,0 +1,10 @@
+describe("DungeonController", function() {
+
+  describe("changeFloor()", function() {
+
+    it("ensures that rooms don't overlap")
+    it("connects every room in the dungeon")
+
+  });
+
+});
