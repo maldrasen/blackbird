6d36a449f93143d4202d36ea9cc33b80c9a570e7
Maldrasen <maldrasen@gmail.com>
2026-02-12 12:08:35 -0600

    Add floor factory and spec

--- application/factories/dungeon/floor-factory.js
+++ application/factories/dungeon/floor-factory.js
@@ -1,14 +1,16 @@
 global.FloorFactory = (function() {
 
-  // floor: (number) The depth of this floor.
+  // depth: (number) The depth of this floor.
   // theme: (string) The dungeon theme for this floor, or randomly picked if null.
-  function build(floor, theme) {
-    if (floor == null) { throw "The floor number is required" }
-    if (theme == null) { theme = pickTheme(floor); }
+  function build(level, theme=null) {
+    if (level == null) { throw "The floor number is required" }
+    if (theme == null) { theme = pickTheme(level); }
+
+    return Floor.build({ level, theme });
   }
 
   function pickTheme(floor) {
-
+    return `stuff:${floor}`
   }
 
   return Object.freeze({
---
+++ test/factories/floor-factory-spec.js
@@ -0,0 +1,8 @@
+describe.only("FloorFactory", function() {
+
+  it("assigns a theme for the current level", function() {
+    const floor = FloorFactory.build(1);
+    expect(floor.getTheme()).to.exist;
+  });
+
+})
