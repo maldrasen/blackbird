9659e7ff191b284ceaa931886d46d3d432f8245d
Maldrasen <maldrasen@gmail.com>
2026-02-22 15:10:36 +0900

    Move character views into their own module

---
+++ application/views/character/character-body-panel.js
@@ -0,0 +1,19 @@
+global.CharacterBodyPanel = (function() {
+
+  function fillAnus() {}
+  function fillBody() {}
+  function fillBreasts() {}
+  function fillCock() {}
+  function fillMouth() {}
+  function fillPussy() {}
+
+  return Object.freeze({
+    fillAnus,
+    fillBody,
+    fillBreasts,
+    fillCock,
+    fillMouth,
+    fillPussy,
+  });
+
+})();
