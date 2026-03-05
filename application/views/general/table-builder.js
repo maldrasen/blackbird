9ba9d2f0d8271dd268654821f4a310c99b921f47
Maldrasen <maldrasen@gmail.com>
2026-03-05 12:20:49 +0900

    Update the table builder to add ids to all table elements

--- application/views/general/table-builder.js
+++ application/views/general/table-builder.js
@@ -9,36 +9,55 @@
  * ```
  * const table = TableBuilder({ classname:'example-table' });
  * table.addRow();
- * table.addCell('horny stuff');
+ * table.addCell('horny stuff', { classname:'horny-stuff' });
  * table.addCell('pleasant things');
  * table.addRow({ classname:'better' });
- * table.addCell('perverted stuff');
+ * table.addCell('perverted stuff', { id:'pervertedStuff' });
  * table.addCell('disgusting things');
  * table.getTable();
  * ```
  */
 global.TableBuilder = function(options) {
+  const tableId = options.id;
   const tableClassname = options.classname;
   const rows = [];
 
   function addRow(options={}) {
-    rows.push({ cells:[], classname:(options.classname||'') });
+    const row = { cells:[] };
+    if (options.id) { row.id = options.id; }
+    if (options.classname) { row.classname = options.classname; }
+    rows.push(row);
   }
 
   function addCell(content, options={}) {
-    rows[rows.length-1].cells.push({ content:content, classname:(options.classname||'') });
+    const cell = { content:content };
+    if (options.id) { cell.id = options.id }
+    if (options.classname) { cell.classname = options.classname; }
+    rows[rows.length-1].cells.push(cell);
   }
 
   function getTable() {
-    let table = tableClassname ? `<table class=${tableClassname}>` : `<table>`
+    let table = `<table`;
+    if (tableId) { table += ` id='${tableId}'`; }
+    if (tableClassname) { table+= ` class='${tableClassname}'`; }
+    table += `>`
+
     rows.forEach(row => {
-      table += row.classname ? `<tr class='${row.classname}'>` : `<tr>`
+      table += `<tr`;
+      if (row.id) { table += ` id='${row.id}'`; }
+      if (row.classname) { table += ` class=${row.classname}` }
+      table += `>`
+
       row.cells.forEach(cell => {
-        table += cell.classname ? `<td class='${cell.classname}'>` : `<td>`
-        table += `${cell.content}</td>`
+        table += `<td`;
+        if (cell.id) { table += ` id='${cell.id}'`; }
+        if (cell.classname) { table += ` class='${cell.classname}'` }
+        table += `>${cell.content}</td>`;
       });
+
       table += `</tr>`
     });
+
     return X.createElement(`${table}</table>`);
   }
 
