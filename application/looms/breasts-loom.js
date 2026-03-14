6d54074b595742fc9e4741992809bef468d90645
Maldrasen <maldrasen@gmail.com>
2026-03-15 08:43:31 +0900

    More planning of eventual loom stuff

--- application/looms/breasts-loom.js
+++ application/looms/breasts-loom.js
@@ -1,5 +1,15 @@
 global.BreastsLoom = (function() {
 
+  // {A:breasts.bigSoftBreasts} A phrase like "big firm tits" or "large soft breasts"
+  // {A:breasts.bigBreasts} A phrase like "big tits" or "large breasts"
+  // {A:breasts.softBreasts} A phrase like "soft tits" or "firm breasts"
+  // {A:breasts.bigRoundBreasts} A phrase describing breast size and shape.
+  // {A:breasts.big} An adjective that can be used to describe the breast size.
+  // {A:breasts.soft} An adjective that can be used to describe the breast firmness
+  // {A:breasts.bigSoft} A longer adjective phrase without a word for breasts.
+  // {A:breasts.bigRound} A longer adjective phrase describing the shape without a word for breasts.
+  // {A:breasts} Any word for breasts.
+  // {A:breasts:thickNipples} A phrase like 'long nipples' or 'dark teats'
   function weave(id, token) {
     throw `Unknown Breasts Token (${token})`;
   }
--- application/looms/cock-loom.js
+++ application/looms/cock-loom.js
@@ -1,5 +1,10 @@
 global.CockLoom = (function() {
 
+  // {A:cock.thickSixInchLongCock}` A long phrase that explicitly includes the length.
+  // {A:cock.sixInch}` A short phrase with just the length.
+  // {A:cock.bigCock}` A phrase like "huge cock" or "big dick"
+  // {A:cock.big}` Just an adjective that can be used to describe the cock.
+  // {A:cock.thick}` An adjective that doesn't rely on size.
   function weave(id, token) {
     throw `Unknown Cock Token (${token})`;
   }
