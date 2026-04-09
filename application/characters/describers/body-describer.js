bd809b58a93131d470146bcb4113d2039ff1d05c
Maldrasen <maldrasen@gmail.com>
2026-04-09 13:42:44 +0700

    Starting species descriptions

--- application/characters/describers/body-describer.js
+++ application/characters/describers/body-describer.js
@@ -1,6 +1,34 @@
 global.BodyDescriber = (function() {
 
-  function getTemplate(id) {}
+  function getTemplate(id) {
+    const parts = [
+      describeSpecies(id),
+      describeHeight(id)];
+  }
+
+  function describeSpecies(id) {
+    switch (ActorComponent.lookup(id).species) {
+      case SpeciesCode.elf: return describeElf(id);
+      case SpeciesCode.equian: return describeEquian(id);
+      case SpeciesCode.halfling: return describeHalfling(id);
+      case SpeciesCode.human: return describeHuman(id);
+      case SpeciesCode.kobold: return describeKobold(id);
+      case SpeciesCode.lupin: return describeLupin(id);
+      case SpeciesCode.nymph: return describeNymph(id);
+      case SpeciesCode.sylph: return describeSylph(id);
+      case SpeciesCode.vermen: return describeVermen(id);
+    }
+  }
+
+  function describeElf(id) {}
+  function describeEquian(id) {}
+  function describeHalfling(id) {}
+  function describeHuman(id) {}
+  function describeKobold(id) {}
+  function describeLupin(id) {}
+  function describeNymph(id) {}
+  function describeSylph(id) {}
+  function describeVermen(id) {}
 
   function describeHeight(id) {
     const actor = ActorComponent.lookup(id);
@@ -11,29 +39,29 @@ global.BodyDescriber = (function() {
     const options = [];
 
     if (tallness < 0.90) {
-      options.push({ text:`{C:name} is much shorter than most {C:species.elven} {C:men}, standing ${measurement} tall.` });
-      options.push({ text:`{C:name} is ${measurement} tall, which is very short for {C:species.anElf} {C:man}.` });
-      options.push({ text:`At ${measurement}, {C:name} is much shorter than most {C:species.elven} {C:men}.` });
+      options.push({ text:`{C:He}'s much shorter than most {C:species.elven} {C:men}, standing ${measurement} tall.` });
+      options.push({ text:`{C:He}'s ${measurement} tall, which is very short for {C:species.anElf} {C:man}.` });
+      options.push({ text:`At ${measurement}, {C:he}'s much shorter than most {C:species.elven} {C:men}.` });
     }
     if (tallness >= 0.90 && tallness < 0.95) {
-      options.push({ text:`{C:name} is shorter than most {C:species.elven} {C:men}, standing ${measurement} tall.` });
-      options.push({ text:`{C:name} is ${measurement} tall, which is somewhat short for {C:species.anElf} {C:man}.` });
-      options.push({ text:`At ${measurement}, {C:name} is a little on the short side, when compared to most {C:species.elven} {C:men}.` });
+      options.push({ text:`{C:He}'s shorter than most {C:species.elven} {C:men}, standing ${measurement} tall.` });
+      options.push({ text:`{C:He}'s ${measurement} tall, which is somewhat short for {C:species.anElf} {C:man}.` });
+      options.push({ text:`At ${measurement}, {C:he}'s a little on the short side, when compared to most {C:species.elven} {C:men}.` });
     }
     if (tallness >= 0.95 && tallness < 1.05) {
-      options.push({ text:`{C:name} is average height for an {C:species.elf} {C:man}, standing ${measurement} tall.` });
-      options.push({ text:`{C:name} is ${measurement} tall, which is about average for {C:species.anElf} {C:man}.` });
-      options.push({ text:`At ${measurement}, {C:name} is about average for an {C:species.elven} {C:man}.` });
+      options.push({ text:`{C:He}'s average height for an {C:species.elf} {C:man}, standing ${measurement} tall.` });
+      options.push({ text:`{C:He}'s ${measurement} tall, which is about average for {C:species.anElf} {C:man}.` });
+      options.push({ text:`At ${measurement}, {C:he}'s about average for an {C:species.elven} {C:man}.` });
     }
     if (tallness >= 1.05 && tallness < 1.10) {
-      options.push({ text:`{C:name} is taller than most {C:species.elven} {C:men}, standing ${measurement} tall.` });
-      options.push({ text:`{C:name} is ${measurement} tall, which is fairly tall for {C:species.anElf} {C:man}.` });
-      options.push({ text:`At ${measurement}, {C:name} is taller than most {C:species.elven} {C:men}.` });
+      options.push({ text:`{C:He}'s taller than most {C:species.elven} {C:men}, standing ${measurement} tall.` });
+      options.push({ text:`{C:He}'s ${measurement} tall, which is fairly tall for {C:species.anElf} {C:man}.` });
+      options.push({ text:`At ${measurement}, {C:he}'s taller than most {C:species.elven} {C:men}.` });
     }
     if (tallness >= 1.10) {
-      options.push({ text:`{C:name} is much taller than most {C:species.elven} {C:men}, standing ${measurement} tall.` });
-      options.push({ text:`{C:name} is ${measurement} tall, which is very tall for {C:species.anElf} {C:man}.` });
-      options.push({ text:`At ${measurement}, {C:name} is much taller than most {C:species.elven} {C:men}.` });
+      options.push({ text:`{C:He}'s much taller than most {C:species.elven} {C:men}, standing ${measurement} tall.` });
+      options.push({ text:`{C:He}'s ${measurement} tall, which is very tall for {C:species.anElf} {C:man}.` });
+      options.push({ text:`At ${measurement}, {C:he}'s much taller than most {C:species.elven} {C:men}.` });
     }
 
     return Random.from(options);
@@ -41,6 +69,7 @@ global.BodyDescriber = (function() {
 
   return Object.freeze({
     getTemplate,
+    describeSpecies,
     describeHeight,
   });
 
--- test/characters/describers/body-describer-spec.js
+++ test/characters/describers/body-describer-spec.js
@@ -1,15 +1,24 @@
-describe("BodyDescriber", function() {
+describe.only("BodyDescriber", function() {
 
   function log(message) {
     console.log(message);
   }
 
+  it("describes species", function() {
+    log(`=== Species ===`);
+    for (let i=0; i<10; i++) {
+      const shadow = CharacterFactory.build({ });
+      const description = BodyDescriber.describeSpecies(shadow);
+      log(Weaver({ C:shadow }).weave(description.text));
+    }
+  });
+
   it("describes height", function() {
     log(`=== Height ===`);
     for (let i=0; i<10; i++) {
-      const elf = CharacterFactory.build({ });
-      const description = BodyDescriber.describeHeight(elf);
-      log(Weaver({ C:elf }).weave(description.text));
+      const shadow = CharacterFactory.build({ });
+      const description = BodyDescriber.describeHeight(shadow);
+      log(Weaver({ C:shadow }).weave(description.text));
     }
   });
 
