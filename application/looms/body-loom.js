db06979f02dd429de6d0792e5195f204f8263207
Maldrasen <maldrasen@gmail.com>
2026-04-10 13:19:15 +0700

    Add body loom
    # Conflicts:
    #	application/looms/body-loom.js

--- application/looms/body-loom.js
+++ application/looms/body-loom.js
@@ -3,20 +3,45 @@ global.BodyLoom = (function() {
   function weave(id, token) {
     const body = BodyComponent.lookup(id);
 
-    if (token === 'eyeColor') {  return body.eyeColor; }
-    if (token === 'furColor') {  return translateHairColor(body.furColor); }
-    if (token === 'hairColor') {  return translateHairColor(body.hairColor); }
+    if (token === 'eyeColor') { return eyeColor(body.eyeColor); }
+    if (token === 'furColor') { return hairColor(body.furColor); }
+    if (token === 'hairColor') { return hairColor(body.hairColor); }
 
     return Weaver.formatWarning(`[Body:${token}]`);
   }
 
-  // The eye colors are all single words, but the hair (and fur) colors have compound words like darkRed. Rather than
-  // mapping all of these using the string helper to translate "darkRed" first into "Dark Red" then to "dark red"
-  // works for all the current hair colors.
-  function translateHairColor(code) {
-    return StringHelper.titlecaseAll(code).toLowerCase();
+  // TODO: This will work fine as is, given that all eye colors are single
+  //   lower case words, but it can be extended like hair colors with more
+  //   color synonyms.
+  function eyeColor(color) {
+    return color;
   }
 
-  return Object.freeze({ weave });
+  function hairColor(color) {
+    if (color === 'platinumBlond') { return Random.from(['platinum blond','bright blond']); }
+    if (color === 'strawberryBlond') { return Random.from(['strawberry blond']); }
+    if (color === 'goldenBlond') { return Random.from(['golden blond']); }
+    if (color === 'darkBlond') { return Random.from(['dark blond','dirty blond','dusty blond']); }
+    if (color === 'lightBrown') { return Random.from(['light brown']); }
+    if (color === 'darkBrown') { return Random.from(['dark brown']); }
+    if (color === 'blackBrown') { return Random.from(['black brown','almost black','nearly black']); }
+    if (color === 'jetBlack') { return Random.from(['jet black','glossy black']); }
+    if (color === 'darkRed') { return Random.from(['dark red']); }
+    if (color === 'lightBlue') { return Random.from(['light blue','powder blue']); }
+    if (color === 'blue') { return Random.from(['blue','bright blue']); }
+    if (color === 'darkBlue') { return Random.from(['dark blue']); }
+    if (color === 'lightGreen') { return Random.from(['light green','pale green']); }
+    if (color === 'green') { return Random.from(['green','sage green','grass green']); }
+    if (color === 'darkGreen') { return Random.from(['dark green','forest green']); }
+    if (color === 'lightPurple') { return Random.from(['light purple','pale purple']); }
+    if (color === 'purple') { return Random.from(['purple','deep purple']); }
+    if (color === 'darkPurple') { return Random.from(['dark purple']); }
+
+    return color;
+  }
+
+  return Object.freeze({
+    weave
+  });
 
 })();
