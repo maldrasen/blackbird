46e04cf363a2d8470a9a1a03849088b519f9b562
Maldrasen <maldrasen@gmail.com>
2026-05-30 14:14:36 -0500

    Modernize the exacto library

--- application/views/exacto.js
+++ application/views/exacto.js
@@ -1,130 +1,151 @@
+global.X = (function() {
 
-// X (or Exacto) is a super tiny replacement for jQuery, just a quick
-// reimplementation of everything I would actually use from that library.
+  function all(query) {
+    return document.querySelectorAll(query);
+  }
 
-global.X = function(query) {
-  return document.querySelectorAll(query)
-}
+  function first(query) {
+    return document.querySelector(query);
+  }
 
-X.first = function(query) {
-  return document.querySelector(query)
-}
+  function body() {
+    return document.getElementsByTagName('body')[0];
+  }
 
-X.body = function() { return document.getElementsByTagName('body')[0]; }
-X.each = function(query, callback) { X(query).forEach(callback); }
+  function each(query, callback) {
+    all(query).forEach(callback);
+  }
 
-// === Create and Modify Elements -=============================================
+  // Remove all elements that match the query string. There's no need for this to only work on a single element, just
+  // call element.remove() instead.
+  function remove(query) {
+    all(query).forEach(element => { element.remove(); });
+  }
 
-// Either remove all elements that match the query string. There's no need for
-// this to only work on a single element, just call element.remove() instead.
-X.remove = function(query) {
-  X(query).forEach(element => { element.remove(); });
-}
+  function removeAttribute(arg, attribute) {
+    (typeof arg === 'string' ? first(arg) : arg).removeAttribute(attribute);
+  }
 
-X.removeAttribute = function(arg, attribute) {
-  (typeof arg === 'string' ? X.first(arg) : arg).removeAttribute(attribute);
-}
+  function empty(arg) {
+    (typeof arg === 'string' ? first(arg) : arg).replaceChildren();
+  }
 
-X.empty = function(arg) {
-  (typeof arg === 'string' ? X.first(arg) : arg).replaceChildren();
-}
+  function fill(arg, element) {
+    (typeof arg === 'string' ? first(arg) : arg).replaceChildren(element);
+  }
 
-X.fill = function(arg, element) {
-  (typeof arg === 'string' ? X.first(arg) : arg).replaceChildren(element);
-}
+  function append(arg, element) {
+    (typeof arg === 'string' ? first(arg) : arg).appendChild(element);
+  }
 
-X.append = function(arg, element) {
-  (typeof arg === 'string' ? X.first(arg) : arg).appendChild(element);
-}
+  function createElement(string) {
+    let element = document.createElement("div");
+    element.innerHTML = string;
+    return element.children[0];
+  }
 
-X.createElement = function(string) {
-  let element = document.createElement("div");
-  element.innerHTML = string;
-  return element.children[0];
-}
+  function copyElement(selector) {
+    return first(selector).cloneNode(true);
+  }
 
-X.copyElement = function(selector) {
-  return X.first(selector).cloneNode(true);
-}
+  function loadDocument(selector, path) {
+    fill(first(selector), createElement(FileHelper.readFile(path)));
+  }
 
-X.loadDocument = function(selector, path) {
-  X.fill(X.first(selector), X.createElement(FileHelper.readFile(path)));
-}
-
-// === Events ==================================================================
-
-X.onClick = function(selector, callback) {
-  window.addEventListener('click', event => {
-    if (event.target.matches(".disabled") === false && event.target.closest(selector)) {
-      callback(event);
-    }
-  });
-}
-
-X.onCodeDown = function(code, when, callback) {
-  let active = false;
-
-  window.addEventListener('keyup', event => {
-    if (active && event.code === code) { active = false; }
-  });
-
-  window.addEventListener('keydown', event => {
-    if (!active && event.code === code) {
-      if (when === true || when(event)) {
-        active = true;
+  function onClick(selector, callback) {
+    window.addEventListener('click', event => {
+      if (event.target.matches(".disabled") === false && event.target.closest(selector)) {
         callback(event);
       }
+    });
+  }
+
+  function onCodeDown(code, when, callback) {
+    let active = false;
+
+    window.addEventListener('keyup', event => {
+      if (active && event.code === code) { active = false; }
+    });
+
+    window.addEventListener('keydown', event => {
+      if (!active && event.code === code) {
+        if (when === true || when(event)) {
+          active = true;
+          callback(event);
+        }
+      }
+    });
+  }
+
+  function onResize(when, callback) {
+    window.addEventListener('resize', (event) => {
+      if (when === true || when(event)) { callback(event); }
+    });
+  }
+
+  // The hasClass() function only makes sense for a single element so it only considers the first element that matches
+  // the selector, which should probably be an ID selector.
+  function hasClass(arg, classname) {
+    const element = typeof arg == "string" ? X.first(arg) : arg;
+    return element ? element.classList.contains(classname) : false;
+  }
+
+  function addClass(arg, classname) {
+    if (typeof arg == "string") {
+      return Array.from(document.querySelectorAll(arg)).forEach((element) => element.classList.add(classname));
     }
+    arg.classList.add(classname);
+  }
+
+  function removeClass(arg, classname) {
+    if (typeof arg == "string") {
+      return Array.from(document.querySelectorAll(arg)).forEach((element) => element.classList.remove(classname));
+    }
+    arg.classList.remove(classname);
+  }
+
+  function removeClassWithin(element, classname) {
+    element.querySelectorAll(`.${classname}`)[0].classList.remove(classname);
+  }
+
+  function getPosition(element) {
+    return element.getBoundingClientRect();
+  }
+
+  function windowDimensions() {
+    return {
+      width: window.innerWidth,
+      height: window.innerHeight,
+    }
+  }
+
+  function assetURL(path) {
+    return `url('${ROOT}/assets/${path}')`;
+  }
+
+  return Object.freeze({
+    all,
+    first,
+    body,
+    each,
+    remove,
+    removeAttribute,
+    empty,
+    fill,
+    append,
+    createElement,
+    copyElement,
+    loadDocument,
+    onClick,
+    onCodeDown,
+    onResize,
+    hasClass,
+    addClass,
+    removeClass,
+    removeClassWithin,
+    getPosition,
+    windowDimensions,
+    assetURL,
   });
-}
 
-X.onResize = function(when, callback) {
-  window.addEventListener('resize', (event) => {
-    if (when === true || when(event)) { callback(event); }
-  });
-}
-
-// === Classes =================================================================
-
-// These class manipulation function can all take either an element or a query
-// selector. With hasClass() the function really only makes sense for a single
-// element so it only considers the first element with the selector, intended
-// for use with element IDs.
-
-X.hasClass = function(arg, classname) {
-  const element = typeof arg == "string" ? X.first(arg) : arg;
-  return element ? element.classList.contains(classname) : false;
-}
-
-X.addClass = function(arg, classname) {
-  if (typeof arg == "string") {
-    return Array.from(document.querySelectorAll(arg)).forEach((element) => element.classList.add(classname));
-  }
-  arg.classList.add(classname);
-}
-
-X.removeClass = function(arg, classname) {
-  if (typeof arg == "string") {
-    return Array.from(document.querySelectorAll(arg)).forEach((element) => element.classList.remove(classname));
-  }
-  arg.classList.remove(classname);
-}
-
-X.removeClassWithin = function(element, classname) {
-  element.querySelectorAll(`.${classname}`)[0].classList.remove(classname);
-}
-
-X.getPosition = function(element) {
-  return element.getBoundingClientRect();
-}
-
-X.windowDimensions = function() {
-  return {
-    width: window.innerWidth,
-    height: window.innerHeight,
-  }
-}
-
-X.assetURL = function(path) {
-  return `url('${ROOT}/assets/${path}')`;
-}
+})();
