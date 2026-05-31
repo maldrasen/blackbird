b2ba6b7095d0299184afd77fa569f2a8a1ac59fc
Maldrasen <maldrasen@gmail.com>
2026-05-31 14:44:36 -0500

    Just some armor ideas

---
+++ scraps/equipment/armor-constructions.js
@@ -0,0 +1,74 @@
+global.ArmorConstructions = {
+
+  leather: {
+    prefix: 'Leather',
+    resistances: {
+      crush: 0,
+      slash: 5,
+      pierce: 2,
+    },
+  },
+
+  studded: {
+    prefix: 'Studded Leather',
+    resistances: {
+      crush: 2,
+      slash: 8,
+      pierce: 6,
+    },
+  },
+
+  ring: {
+    prefix: 'Ringed Leather',
+    resistances: {
+      crush: 2,
+      slash: 10,
+      pierce: 8,
+    },
+  },
+
+  chain: {
+    prefix: 'Chainmail',
+    resistances: {
+      crush: 4,
+     slash: 12,
+      pierce: 10,
+    },
+  },
+
+  scale: {
+    prefix: 'Scaled',
+    resistances: {
+      crush: 15,
+      slash: 15,
+      pierce: 15,
+    },
+  },
+
+  splint: {
+    prefix: 'Splinted',
+    resistances: {
+      crush: 18,
+      slash: 18,
+      pierce: 15,
+    },
+  },
+
+  laminar: {
+    prefix: 'Laminar',
+    resistances: {
+      crush: 20,
+      slash: 20,
+      pierce: 20,
+    }
+  },
+
+  plate: {
+    prefix: 'Plate',
+    resistances: {
+      crush: 25,
+      slash: 25,
+      pierce: 25,
+    }
+  }
+}
---
+++ scraps/equipment/armor-materials.js
@@ -0,0 +1,33 @@
+global.ArmorMaterials = {
+
+  leather: {
+    prefix: 'Leather',
+    type: 'leather',
+    reduction: 2,
+  },
+
+  'minotaur-leather': {
+    prefix: 'Minotaur Leather',
+    type: 'leather',
+    reduction: 5,
+  },
+
+  bronze: {
+    prefix: 'Bronze',
+    type: 'metal',
+    reduction: 5,
+  },
+
+  iron: {
+    prefix: 'Iron',
+    type: 'metal',
+    reduction: 7,
+  },
+
+  steel: {
+    prefix: 'Steel',
+    type: 'metal',
+    reduction: 10,
+  }
+
+}
