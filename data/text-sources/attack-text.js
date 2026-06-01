6b89f2f275a38eb5f6ecb1ebcf5786306bfd77c1
Maldrasen <maldrasen@gmail.com>
2026-06-01 18:17:12 -0500

    Add text sources

---
+++ data/text-sources/attack-text.js
@@ -0,0 +1,8 @@
+
+TextSource.register('basic-swing',{ text:[
+  `{A:name} swings {A:his} {A:weapon-name} at {T:name}.`
+]});
+
+TextSource.register('basic-thrust',{ text:[
+  `{A:name} thrusts {A:his} {A:weapon-name} at {T:name}.`
+]});
