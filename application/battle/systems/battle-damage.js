a8719be74bd2e4ebb3ba8c2ae004bf9608761bba
Maldrasen <maldrasen@gmail.com>
2026-06-18 16:07:51 -0600

    Apply vulnerable to damage

--- application/battle/systems/battle-damage.js
+++ application/battle/systems/battle-damage.js
@@ -6,8 +6,12 @@ global.BattleDamage = (function() {
   //    Data: { entity, damage, damageTypes, hitLocation, isCrit }
   //
   function applyDamage(data) {
-    if (BattleSystem.getState().isAlive(data.entity) === false) {
-      throw new Error(`[${data.entity}] is already dead. Damage should not have been applied.`);
+    const state = BattleSystem.getState();
+    const target = data.entity;
+    const damageTypes = data.damageTypes;
+
+    if (state.isAlive(target) === false) {
+      throw new Error(`[${target}] is already dead. Damage should not have been applied.`);
     }
 
     let actualDamage = 0;
@@ -17,24 +21,32 @@ global.BattleDamage = (function() {
     //       resistances to certain damage types. I don't think we need to modify the damageTypes object here,
     //       because once we get the total actual damage that should be all that matters.
 
-    // TODO: This will also need to take into account conditions like vulnerable which increase the raw damage of all
-    //       damage types (or just doubles the final actual damage)
+    // TODO: How does resistance reduce damage? When rolling to resist an effect we make opposed rolls, so the scales
+    //       of the numbers don't matter. However... if a character has 20 resistance (or a -15) resistance to a damage
+    //       type that should either reduce (or raise) by a percent or do a flat reduction... A flat reduction would
+    //       scale better. Meaning if a character has 20 fire resistance, and took 22 fire damage, they'd actually take
+    //       2 damage, or 0 if the damage was less than 20... Seems overly powerful though. Still, I'm not sure how the
+    //       resistance values will scale at all. Should effect resistance and damage reduction be two different stats?
 
-    Object.entries(data.damageTypes).forEach(([type, damage]) => {
+    Object.entries(damageTypes).forEach(([type, damage]) => {
       actualDamage += damage;
     });
 
-    const health = HealthComponent.lookup(data.entity);
+    if (state.hasStatusEffect(target,'vulnerable')) {
+      actualDamage = actualDamage * 2;
+    }
+
+    const health = HealthComponent.lookup(target);
     health.currentHealth -= actualDamage;
 
     if (health.currentHealth <= 0) {
       health.currentHealth = 0;
       killed = true;
-      killEntity(data.entity);
+      killEntity(target);
     }
 
     BattleInterface.showDamageEffect({ killed, ...data });
-    HealthComponent.update(data.entity, health);
+    HealthComponent.update(target, health);
 
     return actualDamage;
   }
