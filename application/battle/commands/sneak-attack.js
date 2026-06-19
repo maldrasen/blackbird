f1bf1f78d6a026be5a226ede6f3595fdc829c1d4
Maldrasen <maldrasen@gmail.com>
2026-06-19 06:30:22 -0600

    Notes

---
+++ application/battle/commands/sneak-attack.js
@@ -0,0 +1,11 @@
+global.SneakAttack = (function() {
+
+  function execute(attacker, target) {
+    return { messages:[{ text:'TODO: Sneak Attack' }], time:1000 };
+  }
+
+  return Object.freeze({
+    execute,
+  });
+
+})();
