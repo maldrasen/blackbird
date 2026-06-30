15ecbd194d77c49fda03ee1360082f87e60fe493
Maldrasen <maldrasen@gmail.com>
2026-06-30 15:34:08 -0500

    Finding the closest features in two passes wasn't a good idea. Instead we need to compare every feature's position against every other feature's position.

--- application/dungeon/factories/feature-graph-helper.js
+++ application/dungeon/factories/feature-graph-helper.js
@@ -1,56 +1,32 @@
 global.FeatureGraphHelper = (function() {
 
-  // Finding the closest disconnected features is a two step process. First we go through the spanning forest, finding
-  // the two disconnected trees that are closest to each other. We then search through every node in the two trees,
-  // finding the two rooms that are closest to each other. This function returns the two indices of the features.
+  // Finding the closest disconnected features requires comparing every feature in every tree against every feature in
+  // every other tree, and returning the two indices of the closest pair. There might be a more efficient way to do
+  // this, but with a dungeon floor having at most around 100 features, doing 10,000 comparisons doesn't actually take
+  // a noticeable amount of time.
   function getClosestDisconnectedFeatures(forest) {
-    const [first, second] = findClosestIslands(forest);
-    return findClosestFeatures(forest[first], forest[second]);
-  }
-
-  function findClosestFeatures(treeA, treeB) {
     const features = DungeonSystem.getDungeonFloor().getFeatures();
-    const vertsA = treeA.getVertices();
-    const vertsB = treeB.getVertices();
-    const featuresA = vertsA.map(i => features[i].getLocation());
-    const featuresB = vertsB.map(i => features[i].getLocation());
-
-    let leastDistance = 10000;
-    let first;
-    let second;
-
-    for (let a=0; a<featuresA.length; a++) {
-      for (let b=0; b<featuresB.length; b++) {
-        const distance = boxDistance(featuresA[a], featuresB[b]);
-
-        if (distance < leastDistance) {
-          leastDistance = distance;
-          first = vertsA[a];
-          second = vertsB[b];
-        }
-      }
-    }
-
-    return [first,second];
-  }
-
-  function findClosestIslands(forest) {
-    const bounds = forest.map(tree => tree.getBounds());
 
     let leastDistance = 10000;
     let first;
     let second;
 
     for (let i=0; i<forest.length; i++) {
-      for (let j=0; j<forest.length; j++) {
-        if (i !== j) {
-          const distance = boxDistance(bounds[i], bounds[j]);
-          if (distance < leastDistance) {
-            leastDistance = distance;
-            first = i;
-            second = j;
-          }
-        }
+      for (let j=i+1; j<forest.length; j++) {
+        const vertsA = forest[i].getVertices();
+        const vertsB = forest[j].getVertices();
+
+        vertsA.forEach(a => {
+          vertsB.forEach(b => {
+            const distance = boxDistance(features[a].getLocation(), features[b].getLocation());
+
+            if (distance < leastDistance) {
+              leastDistance = distance;
+              first = a;
+              second = b;
+            }
+          });
+        });
       }
     }
 
