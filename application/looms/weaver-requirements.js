40422c7ea7c70ca6cbc325cd6d5022342450baf4
Maldrasen <maldrasen@gmail.com>
2026-07-05 12:27:19 -0500

    Breasts requirement

--- application/looms/weaver-requirements.js
+++ application/looms/weaver-requirements.js
@@ -1,14 +1,13 @@
 global.WeaverRequirements = (function() {
 
-  function visiblePussy(context, key) {
+  function visibleBreasts(context, key) {
     const character = Character(context[key]);
-    return character.hasNormalPussy() && character.isCrotchExposed();
+    return character.hasBreasts() && character.areBreastsExposed();
   }
 
   function visibleCock(context, key) {
     const character = Character(context[key]);
     return character.hasNormalCock() && character.isCrotchExposed();
-
   }
 
   function visibleHardCock(context, key) {
@@ -16,12 +15,18 @@ global.WeaverRequirements = (function() {
     return character.isFullyErect() && character.isCrotchExposed();
   }
 
+  function visiblePussy(context, key) {
+    const character = Character(context[key]);
+    return character.hasNormalPussy() && character.isCrotchExposed();
+  }
+
   return Object.freeze({
     playerIs: key => { return (context) => { return GameState.getPlayer() === context[key] }},
     withAttitude: code => { return (context) => { return context.attitude === code }},
-    visiblePussy: key => { return (context) => { return visiblePussy(context,key); }},
+    visibleBreasts: key => { return (context) => { return visibleBreasts(context,key); }},
     visibleCock: key => { return (context) => { return visibleCock(context,key); }},
     visibleHardCock: key => { return (context) => { return visibleHardCock(context,key) }},
+    visiblePussy: key => { return (context) => { return visiblePussy(context,key); }},
   });
 
 })();
