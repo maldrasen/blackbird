4ea15b15d9888fd79af82ba0b204f3f5ba0d31b3
Maldrasen <maldrasen@gmail.com>
2026-07-05 17:44:57 -0500

    Update propose training for bastard

--- data/dialog/bastard/propose-training.js
+++ data/dialog/bastard/propose-training.js
@@ -3,89 +3,62 @@
 // I wanted to separate bastards and bitches though as there's a definite gender difference between the two dialog
 // trees.
 
+const legsCovered = WeaverRequirements.legsAreCovered('T');
+const bigCock = WeaverRequirements.minimumCockSize('T', 'big');
+const taller = WeaverRequirements.isTallerThan('T', 'P');
+const strong = WeaverRequirements.minimumStrength('T', 20);
+const wouldSuckWillingly = WeaverRequirements.wouldConsentTo('T', 'suck-cock', Consent.willing);
+const wouldSuckReluctantly = WeaverRequirements.wouldConsentTo('T', 'suck-cock', Consent.reluctant);
+
 const eager = WeaverPackage('bastard.propose-training.eager');
 const willing = WeaverPackage('bastard.propose-training.willing');
 const reluctant = WeaverPackage('bastard.propose-training.reluctant');
 const unwilling = WeaverPackage('bastard.propose-training.unwilling');
 
-Dialog.register(ArchetypeCode.bastard, DialogKeys.proposeTraining_Eager, context => {
-  const partner = Character(context.T);
-  const options = [];
+Dialog.register(ArchetypeCode.bastard, DialogKeys.proposeTraining_Eager, eager);
+Dialog.register(ArchetypeCode.bastard, DialogKeys.proposeTraining_Willing, willing);
+Dialog.register(ArchetypeCode.bastard, DialogKeys.proposeTraining_Reluctant, reluctant);
+Dialog.register(ArchetypeCode.bastard, DialogKeys.proposeTraining_Unwilling, unwilling);
 
-  if (partner.isEquipped(EquipmentSlot.legs) && partner.cockIsAtLeast('big')) {
-    options.push(`{T:name} grabs your wrist roughly and presses your palm against his 
-      thick bulge. "You want to train this? Ha! Bend over then {T:meanName}."`)
 
-    if (partner.isTallerThan(context.P)) {
-      options.push(`{T:name} smirks down at you, one hand lazily tightening around the thick bulge in his
-        {T:equipped.legs}. "Heh, Fine... train me. But don't think for a second I'll make this easy on you."`);
-    }
-  }
 
-  if (partner.isStrongerThan(20) && partner.isTallerThan(context.P)) {
-    options.push(`{T:name} steps in close, letting you feel the heat radiating off 
-      of his broad frame. "Yeah, go ahead and try to break me. I could use the laugh."`)
-    options.push(`{T:name} chuckles low, rolling his shoulders so his muscles 
-      flex under his shirt. "Alright, {T:meanName}. Train away."`)
-  }
+eager.add(`{T:name} grabs your wrist roughly and presses your palm against his thick bulge. "You want to train this? 
+  Ha! Bend over then {T:meanName}."`,
+  [legsCovered, bigCock]);
+eager.add(`{T:name} smirks down at you, one hand lazily tightening around the thick bulge in his {T:equipped.legs}.
+  "Heh, Fine... train me. But don't think for a second I'll make this easy on you."`,
+  [legsCovered, bigCock, taller]);
+eager.add(`{T:name} steps in close, letting you feel the heat radiating off of his broad frame. "Yeah, go ahead and
+   try to break me. I could use the laugh."`,
+  [strong, taller]);
+eager.add(`{T:name} chuckles low, rolling his shoulders so his muscles flex under his shirt. "Alright, {T:meanName}. 
+  Train away."`,
+  [strong, taller]);
+eager.add(`{T:name} leans against the wall with his arms crossed, his cocky grin never fading. "Heh, fine... I'll 
+  still be the one making you beg by the end."`);
 
-  options.push(`{T:name} leans against the wall with his arms crossed, his cocky grin 
-    never fading. "Heh, fine... I'll still be the one making you beg by the end."`)
 
-  return Random.from(options);
-});
 
-Dialog.register(ArchetypeCode.bastard, DialogKeys.proposeTraining_Willing, context => {
-  const partner = Character(context.T);
-  const options = [];
+willing.add(`{T:name} grins and hooks a thumb in his waistband, tugging it low enough to show the root of his
+  {T:cock.thick} cock. "All right, I'll let you suck me off"`,
+  [legsCovered, wouldSuckWillingly]);
+willing.add(`{T:name} chuckles and cracks his neck, the sound sharp, then fixes you with a bored stare.
+  "Fine. I'll let you play with me. Just don't cry when I take charge instead."`,
+  strong);
+willing.add(`{T:name} shrugs, looking you up and down. "Sure, we can let you pretend you're in charge for a bit."`);
 
-  if (partner.isEquipped(EquipmentSlot.legs) && partner.wouldConsentTo('suck-cock')) {
-    options.push(`{T:name} grins and hooks a thumb in his waistband, tugging it low enough to 
-      show the root of his {T:cock.thick} cock. "All right, I'll let you suck me off"`)
-  }
 
-  if (partner.isStrongerThan(20)) {
-    options.push(`{T:name} chuckles and cracks his neck, the sound sharp, then fixes you with a 
-      bored stare. "Fine. I'll let you play with me. Just don't cry when I take charge instead."`)
-  }
 
-  options.push(`{T:name} shrugs, looking you up and down. "Sure, we can
-    let you pretend you're in charge for a bit."`)
+reluctant.add(`{T:name} rubs the back of his neck, scowling. "Ugh, fine... I guess I could let you suck me off"`,
+  wouldSuckReluctantly);
+reluctant.add(`{T:name} snorts, crossing his arms over his chest. "Yeah? All right {T:meanName}, let's see what you got."`);
+reluctant.add(`{T:name} shifts his weight, his jaw tight. "This is beneath me... but whatever. Just keep your voice 
+  down when I start pounding you."`);
 
-  return Random.from(options);
-});
 
-Dialog.register(ArchetypeCode.bastard, DialogKeys.proposeTraining_Reluctant, context => {
-  const partner = Character(context.T);
-  const options = [];
 
-  if (partner.wouldConsentTo('suck-cock', Consent.reluctant)) {
-    options.push(`{T:name} rubs the back of his neck, scowling. "Ugh, fine... I could let you suck me off"`);
-  }
-
-  options.push(`{T:name} snorts, crossing his arms over his chest. 
-    "Yeah? All right {T:meanName}, let's see what you got."`)
-  options.push(`{T:name} shifts his weight, his jaw tight. "This is beneath me... 
-    but whatever. Just keep your voice down when I start pounding you."`)
-
-  return Random.from(options);
-});
-
-Dialog.register(ArchetypeCode.bastard, DialogKeys.proposeTraining_Unwilling, context => {
-  const partner = Character(context.T);
-  const options = [];
-
-  if (partner.isEquipped(EquipmentSlot.legs) && partner.cockIsAtLeast('big')) {
-    options.push(`{T:name} sneers, adjusting the thick bulge in his pants with 
-      zero shame. "You want some of this bitch? Heh, you wish."`)
-  }
-  if (partner.isTallerThan(context.P)) {
-    options.push(`{T:name} laughs, stepping closer so his full height towers
-      over you. "You? Train me? Heh, cute. Fuck off {T:meanName}."`)
-  }
-
-  options.push(`{T:name} turns away from you, dismissing you with a wave. "I'm not 
-    some bitch you can collar. Take your little fantasy somewhere else."`)
-
-  return Random.from(options);
-});
+unwilling.add(`{T:name} sneers, adjusting the thick bulge in his pants with zero shame. "You want some of this bitch? Heh, you wish."`,
+  [legsCovered, bigCock]);
+unwilling.add(`{T:name} laughs, stepping closer so his full height towers over you. "You? Train me? Heh, cute. Fuck off {T:meanName}."`,
+  [taller]);
+unwilling.add(`{T:name} turns away from you, dismissing you with a wave. "I'm not some bitch you can collar. Take this little fantasy of yours somewhere else."`);
--- data/dialog/bimbo/propose-training.js
+++ data/dialog/bimbo/propose-training.js
@@ -2,169 +2,184 @@
 // characters never get too smart. Bimbos will also always be females or futas so we know they at least have tits,
 // though not always necessarily large tits.
 
-Dialog.register(ArchetypeCode.bimbo, DialogKeys.proposeTraining_Eager, context => {
-  const player = Character(context.P);
-  const partner = Character(context.T);
+const pHasCock = WeaverRequirements.hasCock('P');
+const pBigCock = WeaverRequirements.minimumCockSize('P', 'big');
+const pHugeCock = WeaverRequirements.minimumCockSize('P', 'huge');
 
-  if (player.hasNormalCock()) {
-    if (partner.breastsAreAtLeast('huge') && partner.areBreastsExposed() &&
-      partner.wouldConsentTo('get-titfuck') && Random.roll(10) < 1) {
-      return `{T:name} grins widely and grabs {T:his} {T:breasts.bigRoundBreasts}, squeezing them 
-        together roughly, "Mmm, yes! I want you to fuck my titties really hard this time."`
+const tHasCock = WeaverRequirements.hasCock('T');
+const tVisibleCock = WeaverRequirements.visibleCock('T');
+const tBigBreasts = WeaverRequirements.minimumBreastSize('T', 'big');
+const tHugeBreasts = WeaverRequirements.minimumBreastSize('T', 'huge');
+const tBreastsVisible = WeaverRequirements.visibleBreasts('T');
+const tConsentsTitfuck = WeaverRequirements.wouldConsentTo('T', 'get-titfuck');
+const tConsentsBlowjob = WeaverRequirements.wouldConsentTo('T', 'get-blowjob');
+const tConsentsRimming = WeaverRequirements.wouldConsentTo('T', 'get-rimming');
+const tAnalSlut = WeaverRequirements.hasSexualPreference('T', 'anal-slut', 30);
+const tAnusEmpty = WeaverRequirements.isAnusEmpty('T');
+const tCockLover = WeaverRequirements.hasSexualPreference('T', 'cock-lover', 20);
+const tChestEquipped = WeaverRequirements.chestIsCovered('T');
+
+// Bimbo dialog is built from a motion fragment and a dialogue fragment that are each picked independently, so the
+// same handful of authored lines can recombine into a lot of different scenes. This helper joins the two packages
+// the way the propose-training episode expects the picked template to look.
+function motionAndDialogue(motion, dialogue) {
+  return { pick: context => `${motion.pick(context)} "${dialogue.pick(context)}"` };
+}
+
+// Eager also has a couple of rare full-line moments that replace the motion+dialogue combo entirely. They're gated
+// behind a 1-in-10 roll on top of their own requirements, and fall back to the normal combo if the roll misses or
+// nothing currently qualifies.
+function motionAndDialogueWithSpecial(special, motion, dialogue) {
+  return { pick: context => {
+    if (Random.roll(10) < 1) {
+      try { return special.pick(context); } catch (err) { /* nothing qualifies right now, fall through */ }
     }
-    if (partner.hasSexualPreference('cock-lover') && partner.wouldConsentTo('get-blowjob') && Random.roll(10)<1) {
-      return `{setPosition(kneeling-dominant)} {T:name} grins and drops to {T:his} knees. 
-        {T:He} opens his mouth wide and says, "Ahhh...."`
-    }
-  }
+    return `${motion.pick(context)} "${dialogue.pick(context)}"`;
+  }};
+}
 
-  const motionOptions = [
-    `{T:name} presses {T:his} soft body against you, pushing your arm between {T:his} {T:breasts.bigSoftBreasts}.`,
-    `{T:name} bounces on {T:his} heels, eyes lighting up with excitement.`,
-    `{T:name} twirls a lock of {T:his} hair around a finger while giggling.`,
-    `{T:name's} eyes widen with delight as {T:he} starts to rubs {T:his} thighs together.`];
-  const dialogOptions = [
-    `Time to make me cum again? Okay {T:niceName}.`];
 
-  if (partner.hasSexualPreference('anal-slut',30) && partner.isAnusEmpty()) {
-    dialogOptions.push(`Please {T:niceName}, my little butthole feels so empty right now.`);
-    dialogOptions.push(`Oh yes, I need something really thick in my ass {T:niceName}.`);
-  }
 
-  if (partner.breastsAreAtLeast('big')) {
-    motionOptions.push(`{T:name} bounces in place, {T:his} {T:breasts.bigBreasts} bouncing enticingly.`);
-    motionOptions.push(`The bimbo claps {T:his} hands excitedly, {T:his} {T:breasts.bigBreasts} bouncing wildly.`);
-    motionOptions.push(`{T:name} skips closer, {T:his} ample cleavage heaving with each step.`); }
+const eagerSpecial = WeaverPackage('bimbo.propose-training.eager.special');
+const eagerMotion = WeaverPackage('bimbo.propose-training.eager.motion');
+const eagerDialogue = WeaverPackage('bimbo.propose-training.eager.dialogue');
 
-  if (player.cockIsAtLeast('big')) {
-    dialogOptions.push(`Yes {T:niceName}, I can't wait to feel your {P:cock.bigCock} stretching me open.`); }
+const willingMotion = WeaverPackage('bimbo.propose-training.willing.motion');
+const willingDialogue = WeaverPackage('bimbo.propose-training.willing.dialogue');
 
-  if (player.hasNormalCock() && partner.wouldConsentTo('get-blowjob')) {
-    dialogOptions.push(`Hehehe. {T:niceName} needs {P:his} {cock} sucked again?`);
-    dialogOptions.push(`Ohmygod, yes! I wanna be your favorite little cock sucker!`); }
+const reluctantMotion = WeaverPackage('bimbo.propose-training.reluctant.motion');
+const reluctantDialogue = WeaverPackage('bimbo.propose-training.reluctant.dialogue');
 
-  if (partner.wouldConsentTo('get-rimming')) {
-    dialogOptions.push(`Hehe. Sure thing {T:niceName}. You can make me lick your butthole if you want to.`); }
+const unwillingMotion = WeaverPackage('bimbo.propose-training.unwilling.motion');
+const unwillingDialogue = WeaverPackage('bimbo.propose-training.unwilling.dialogue');
 
-  if (partner.hasNormalCock()) {
-    dialogOptions.push(`Ohmygod, yes {T:niceName}! I wanna be your favorite cum-spraying little slut!`)
+Dialog.register(ArchetypeCode.bimbo, DialogKeys.proposeTraining_Eager,
+  motionAndDialogueWithSpecial(eagerSpecial, eagerMotion, eagerDialogue));
+Dialog.register(ArchetypeCode.bimbo, DialogKeys.proposeTraining_Willing,
+  motionAndDialogue(willingMotion, willingDialogue));
+Dialog.register(ArchetypeCode.bimbo, DialogKeys.proposeTraining_Reluctant,
+  motionAndDialogue(reluctantMotion, reluctantDialogue));
+Dialog.register(ArchetypeCode.bimbo, DialogKeys.proposeTraining_Unwilling,
+  motionAndDialogue(unwillingMotion, unwillingDialogue));
 
-    if (partner.isCrotchExposed()) {
-      motionOptions.push(`{T:name} grins and bounces on {T:his} heels, {T:his} {T:breasts.bigRound} tits 
-        bouncing while {T:his} {T:cock.thickSixInchLongCock} swings heavily between {T:his} thighs.`);
-      motionOptions.push(`{T:name} grins and presses {T:his} body against you, letting 
-        {T:his} {T:cock.thickSixInchLongCock} press firmly against your leg.`)
-    }
-  }
 
-  console.log(`Eager bimbo dialog counts: Motion:${motionOptions.length} Dialog:${dialogOptions.length}`)
 
-  return `${Random.from(motionOptions)} "${Random.from(dialogOptions)}"`;
-});
+eagerSpecial.add(`{T:name} grins widely and grabs {T:his} {T:breasts.bigRoundBreasts}, squeezing them together
+  roughly, "Mmm, yes! I want you to fuck my titties really hard this time."`,
+  [pHasCock, tHugeBreasts, tBreastsVisible, tConsentsTitfuck]);
+eagerSpecial.add(`{setPosition(kneeling-dominant)} {T:name} grins and drops to {T:his} knees. {T:He} opens his mouth
+  wide and says, "Ahhh...."`,
+  [pHasCock, tCockLover, tConsentsBlowjob]);
 
-Dialog.register(ArchetypeCode.bimbo, DialogKeys.proposeTraining_Willing, context => {
-  const player = Character(context.P);
-  const partner = Character(context.T);
 
-  const motionOptions = [
-    `{T:name} blinks at you, {T:his} full lips pouting slightly before curving into a grin.`,
-    `{T:name} tilts {T:his} head, {T:his} round ass shifting as {T:he} smiles dumbly.`,
-    `{T:name} absently plays with a strand of {T:his} long {T:body.hairColor} hair.`,
-    `{T:name} grins and twirls a strand of {T:body.hairColor} hair around {T:his} finger.`];
-  const dialogOptions = [
-    `Hehe, okay {T:niceName}. I love feeling all filled up and stuff.`,
-    `Umm, sure! That sounds fun.`];
 
-  if (player.hasNormalCock()) {
-    dialogOptions.push(`Um, okay! If it's your {cock}, I guess I'm in. Or umm... it's in!`)
-    dialogOptions.push(`Um, sure. You wanna feel my tight little holes around your cock, don't you {T:niceName}?`);
-  }
+eagerMotion.add(`{T:name} presses {T:his} soft body against you, pushing your arm between {T:his} {T:breasts.bigSoftBreasts}.`);
+eagerMotion.add(`{T:name} bounces on {T:his} heels, eyes lighting up with excitement.`);
+eagerMotion.add(`{T:name} twirls a lock of {T:his} hair around a finger while giggling.`);
+eagerMotion.add(`{T:name's} eyes widen with delight as {T:he} starts to rubs {T:his} thighs together.`);
+eagerMotion.add(`{T:name} bounces in place, {T:his} {T:breasts.bigBreasts} bouncing enticingly.`,
+  [tBigBreasts]);
+eagerMotion.add(`The bimbo claps {T:his} hands excitedly, {T:his} {T:breasts.bigBreasts} bouncing wildly.`,
+  [tBigBreasts]);
+eagerMotion.add(`{T:name} skips closer, {T:his} ample cleavage heaving with each step.`,
+  [tBigBreasts]);
+eagerMotion.add(`{T:name} grins and bounces on {T:his} heels, {T:his} {T:breasts.bigRound} tits bouncing while
+  {T:his} {T:cock.thickSixInchLongCock} swings heavily between {T:his} thighs.`,
+  [tVisibleCock]);
+eagerMotion.add(`{T:name} grins and presses {T:his} body against you, letting {T:his} {T:cock.thickSixInchLongCock}
+  press firmly against your leg.`,
+  [tVisibleCock]);
 
-  if (partner.breastsAreAtLeast('big')) {
-    dialogOptions.push(`Um, okay. Did you want to feel how heavy my boobs are?`);
-  }
 
-  if (partner.isEquipped('chest')) {
-    motionOptions.push(`The bimbo nods slowly, {T:his} vacant eyes glazing over as {T:he}
-      adjusts {T:his} top, "accidentally" exposing one of {T:his} {T:breasts:thickNipples}.`);
-  }
 
-  if (partner.hasSexualPreference('anal-slut',30) && partner.isAnusEmpty()) {
-    dialogOptions.push(`Um sure... do you think you could put something like... in my butt.`);}
+eagerDialogue.add(`Time to make me cum again? Okay {T:niceName}.`);
+eagerDialogue.add(`Please {T:niceName}, my little butthole feels so empty right now.`,
+  [tAnalSlut, tAnusEmpty]);
+eagerDialogue.add(`Oh yes, I need something really thick in my ass {T:niceName}.`,
+  [tAnalSlut, tAnusEmpty]);
+eagerDialogue.add(`Yes {T:niceName}, I can't wait to feel your {P:cock.bigCock} stretching me open.`,
+  [pBigCock]);
+eagerDialogue.add(`Hehehe. {T:niceName} needs {P:his} {cock} sucked again?`,
+  [pHasCock, tConsentsBlowjob]);
+eagerDialogue.add(`Ohmygod, yes! I wanna be your favorite little cock sucker!`,
+  [pHasCock, tConsentsBlowjob]);
+eagerDialogue.add(`Hehe. Sure thing {T:niceName}. You can make me lick your butthole if you want to.`,
+  [tConsentsRimming]);
+eagerDialogue.add(`Ohmygod, yes {T:niceName}! I wanna be your favorite cum-spraying little slut!`,
+  [tHasCock]);
 
-  return `${Random.from(motionOptions)} "${Random.from(dialogOptions)}"`;
-});
 
-Dialog.register(ArchetypeCode.bimbo, DialogKeys.proposeTraining_Reluctant, context => {
-  const player = Character(context.P);
-  const partner = Character(context.T);
 
-  const motionOptions = [
-    `{T:name} shifts uncomfortably, looking you over with a blank expression.`,
-    `{T:name} tilts {T:his} head, looking a little confused.`,
-    `{T:name} glances away shyly, pretending to be interested in something off in the distance.`,
-    `The bimbo hesitates, biting {T:his} lip as {T:his} cheeks flush.`];
+willingMotion.add(`{T:name} blinks at you, {T:his} full lips pouting slightly before curving into a grin.`);
+willingMotion.add(`{T:name} tilts {T:his} head, {T:his} round ass shifting as {T:he} smiles dumbly.`);
+willingMotion.add(`{T:name} absently plays with a strand of {T:his} long {T:body.hairColor} hair.`);
+willingMotion.add(`{T:name} grins and twirls a strand of {T:body.hairColor} hair around {T:his} finger.`);
+willingMotion.add(`The bimbo nods slowly, {T:his} vacant eyes glazing over as {T:he} adjusts {T:his} top,
+  "accidentally" exposing one of {T:his} {T:breasts:thickNipples}.`,
+  [tChestEquipped]);
 
-  const dialogOptions = [
-    `Umm... I don't wanna mess it up, but okay. If you really want to.`,
-    `Umm... I dunno, my pussy's a little scared.`,
-    `I guess? I mean... it's kind of a lot. But okay, sure.`,
-    `Training? Umm but... Aren't I good at it already?`,
-    `Umm... sure. But be careful with my titties okay?`];
 
-  if (partner.breastsAreAtLeast('big')) {
-    dialogOptions.push(`You really like my boobies a lot huh? Okay... you can play with them for a little while.`);
-  }
 
-  if (partner.hasSexualPreference('anal-slut',30) && partner.isAnusEmpty()) {
-    dialogOptions.push(`Hmm... okay. But only because my butthole feels so empty right now.`);}
+willingDialogue.add(`Hehe, okay {T:niceName}. I love feeling all filled up and stuff.`);
+willingDialogue.add(`Umm, sure! That sounds fun.`);
+willingDialogue.add(`Um, okay! If it's your {cock}, I guess I'm in. Or umm... it's in!`,
+  [pHasCock]);
+willingDialogue.add(`Um, sure. You wanna feel my tight little holes around your cock, don't you {T:niceName}?`,
+  [pHasCock]);
+willingDialogue.add(`Um, okay. Did you want to feel how heavy my boobs are?`,
+  [tBigBreasts]);
+willingDialogue.add(`Um sure... do you think you could put something like... in my butt.`,
+  [tAnalSlut, tAnusEmpty]);
 
-  if (partner.hasSexualPreference('cock-lover',20) && player.hasNormalCock()) {
-    dialogOptions.push(`Well... your cock does look yummy...`); }
 
-  if (partner.hasNormalCock()) {
-    dialogOptions.push(`Umm... it's a little messy though right? Like when you make me 
-      spray cum everywhere. And then I'm the one who has to clean it all up.`); }
 
-  if (partner.hasNormalCock() && player.cockIsAtLeast('big')) {
-    dialogOptions.push(`Umm... maybe? I mean, my cock wants some, but my little pussy's scared.`); }
+reluctantMotion.add(`{T:name} shifts uncomfortably, looking you over with a blank expression.`);
+reluctantMotion.add(`{T:name} tilts {T:his} head, looking a little confused.`);
+reluctantMotion.add(`{T:name} glances away shyly, pretending to be interested in something off in the distance.`);
+reluctantMotion.add(`The bimbo hesitates, biting {T:his} lip as {T:his} cheeks flush.`);
+reluctantMotion.add(`{T:name} reaches down, wrapping {T:his} hand around {T:his} {T:cock.thickSixInchLongCock},
+  giving it a firm squeeze.`,
+  [tVisibleCock]);
+reluctantMotion.add(`{T:name} shrugs, reaching up to cup {T:his} {T:breasts.bigRoundBreasts}, giving {T:his}
+  {T:breasts.thickNipples} a gentle pull.`,
+  [tBreastsVisible]);
 
-  if (partner.hasNormalCock() && partner.isCrotchExposed()) {
-    motionOptions.push(`{T:name} reaches down, wrapping {T:his} hand around {T:his} 
-      {T:cock.thickSixInchLongCock}, giving it a firm squeeze.`)
-  }
-  if (partner.areBreastsExposed()) {
-    motionOptions.push(`{T:name} shrugs, reaching up to cup {T:his} {T:breasts.bigRoundBreasts}, 
-      giving {T:his} {T:breasts.thickNipples} a gentle pull.` )
-  }
 
-  return `${Random.from(motionOptions)} "${Random.from(dialogOptions)}"`;
-});
 
-Dialog.register(ArchetypeCode.bimbo, DialogKeys.proposeTraining_Unwilling, context => {
-  const player = Character(context.P);
-  const partner = Character(context.T);
+reluctantDialogue.add(`Umm... I don't wanna mess it up, but okay. If you really want to.`);
+reluctantDialogue.add(`Umm... I dunno, my pussy's a little scared.`);
+reluctantDialogue.add(`I guess? I mean... it's kind of a lot. But okay, sure.`);
+reluctantDialogue.add(`Training? Umm but... Aren't I good at it already?`);
+reluctantDialogue.add(`Umm... sure. But be careful with my titties okay?`);
+reluctantDialogue.add(`You really like my boobies a lot huh? Okay... you can play with them for a little while.`,
+  [tBigBreasts]);
+reluctantDialogue.add(`Hmm... okay. But only because my butthole feels so empty right now.`,
+  [tAnalSlut, tAnusEmpty]);
+reluctantDialogue.add(`Well... your cock does look yummy...`,
+  [tCockLover, pHasCock]);
+reluctantDialogue.add(`Umm... it's a little messy though right? Like when you make me spray cum everywhere.
+  And then I'm the one who has to clean it all up.`,
+  [tHasCock]);
+reluctantDialogue.add(`Umm... maybe? I mean, my cock wants some, but my little pussy's scared.`,
+  [tHasCock, pBigCock]);
 
-  const motionOptions = [
-    `{T:name} tilts {T:his} head, {T:his} plump lips forming a frown.`,
-    `{T:name} sticks {T:his} tongue out at you, {T:his} ass jiggling as {T:he} turns away.`,
-    `{T:name} pouts and shakes {T:his} head.`];
-  const dialogOptions = [
-    `Nope. Not with you. My pussy doesn't wanna.`,
-    `No, I don't wanna. I've got like, a headache or something.`];
 
-  if (partner.breastsAreAtLeast('big')) {
-    motionOptions.push(`{T:name} crosses {T:his} arms under {T:his} {T:breasts:bigBreasts}, 
-      pushing them up as though daring you to try something.`);
-    motionOptions.push(`The bimbo shakes {T:his} head, {T:his} {T:breasts:bigBreasts} 
-      swinging back and forth hypnotically.`);
-  }
 
-  if (partner.hasNormalCock() && partner.isCrotchExposed()) {
-    motionOptions.push(`{T:name} shakes {T:his} head, {T:his} {cock} hanging limp between {T:his} legs.`); }
+unwillingMotion.add(`{T:name} tilts {T:his} head, {T:his} plump lips forming a frown.`);
+unwillingMotion.add(`{T:name} sticks {T:his} tongue out at you, {T:his} ass jiggling as {T:he} turns away.`);
+unwillingMotion.add(`{T:name} pouts and shakes {T:his} head.`);
+unwillingMotion.add(`{T:name} crosses {T:his} arms under {T:his} {T:breasts:bigBreasts}, pushing them up as though
+  daring you to try something.`,
+  [tBigBreasts]);
+unwillingMotion.add(`The bimbo shakes {T:his} head, {T:his} {T:breasts:bigBreasts} swinging back and forth
+  hypnotically.`,
+  [tBigBreasts]);
+unwillingMotion.add(`{T:name} shakes {T:his} head, {T:his} {cock} hanging limp between {T:his} legs.`,
+  [tVisibleCock]);
 
-  if (player.cockIsAtLeast('huge')) {
-    dialogOptions.push(`You're too big. Like, find someone else to stretch out.`); }
 
-  return `${Random.from(motionOptions)} "${Random.from(dialogOptions)}"`;
-});
+
+unwillingDialogue.add(`Nope. Not with you. My pussy doesn't wanna.`);
+unwillingDialogue.add(`No, I don't wanna. I've got like, a headache or something.`);
+unwillingDialogue.add(`You're too big. Like, find someone else to stretch out.`,
+  [pHugeCock]);
