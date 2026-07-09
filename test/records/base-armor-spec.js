544830cce2ed91f903a0d202be57e40c9fc2e712
Maldrasen <maldrasen@gmail.com>
2026-07-09 11:39:19 -0500

    Specs todo

---
+++ test/records/base-armor-spec.js
@@ -0,0 +1,23 @@
+describe("BaseArmor", function() {
+
+  describe("getReduction()", function() {
+
+  });
+
+  describe("getReductionMap()", function() {
+
+  });
+
+  describe("getMaterialParts()", function() {
+
+  });
+
+  describe("getPrimaryMaterial()", function() {
+
+  });
+
+  describe("getValue()", function() {
+
+  });
+
+});
---
+++ test/records/base-weapon-spec.js
@@ -0,0 +1,31 @@
+describe("BaseWeapon", function() {
+
+  describe("calculated damage range", function() {
+    // getLow() and getHigh()
+  });
+
+  describe("getDamagePerSecond", function() {
+
+  });
+
+  describe("getMaterialParts", function() {
+
+  });
+
+  describe("getPrimaryMaterial", function() {
+
+  });
+
+  describe("getDamageStat", function() {
+
+  });
+
+  describe("getDamageFactor", function() {
+
+  });
+
+  describe("getValue", function() {
+
+  });
+
+});
