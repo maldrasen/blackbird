12e086d579448594d4305597cd865085b5986552
Maldrasen <maldrasen@gmail.com>
2026-07-24 10:40:03 -0500

    Move overlay into it's own module

--- application/views/enlighten/enlighten-view.js
+++ application/views/enlighten/enlighten-view.js
@@ -5,8 +5,6 @@ global.EnlightenView = (function() {
   function init() {
     X.onClick('#enlightenView .continue-button', finish);
     X.onClick('#enlightenView .level-up-button', openLevelUp);
-    X.onClick('#levelUpOverlay .attribute-pick', pickAttribute);
-    X.onClick('#levelUpOverlay .confirm-button', confirmLevelUp);
   }
 
   function show() {
@@ -137,56 +135,11 @@ global.EnlightenView = (function() {
     };
   }
 
-  // ========================
-  //    Level Up Overlay
-  // ========================
-
   function openLevelUp(event) {
-    const id = event.target.closest('.level-up-button').dataset.id;
-    GeneralOverlay.open(buildLevelUpContent(id), { classname:'small', hideFooter:true });
+    LevelUpOverlay.open(event.target.closest('.level-up-button').dataset.id);
   }
 
-  function buildLevelUpContent(id) {
-    const experience = ExperienceComponent.lookup(id);
-    const attributes = AttributesComponent.lookup(id);
-
-    const content = X.createElement(`<div id='levelUpOverlay' data-id='${id}'>
-      <div class='title'>${Character(id).getName()}</div>
-      <p class='summary'>Level ${experience.level} → ${experience.level + 1}. Choose an attribute to raise.</p>
-      <ul class='attribute-picks'></ul>
-      <div class='button-row align-right'>
-        <a href='#' class='button button-primary confirm-button disabled'>Confirm</a>
-      </div>
-    </div>`);
-
-    Object.keys(Attrib).forEach(code => {
-      X.append(content.querySelector('.attribute-picks'), X.createElement(`<li>
-        <a href='#' class='attribute-pick' data-attribute='${code}'>
-          <span class='attribute-name'>${StringHelper.titlecase(code)}</span>
-          <span class='attribute-value'>${attributes[code]}</span>
-        </a>
-      </li>`));
-    });
-
-    return content;
-  }
-
-  function pickAttribute(event) {
-    X.removeClass('#levelUpOverlay .attribute-pick','selected');
-    X.addClass(event.target.closest('.attribute-pick'),'selected');
-    X.removeClass('#levelUpOverlay .confirm-button','disabled');
-  }
-
-  function confirmLevelUp() {
-    const id = X.first('#levelUpOverlay').dataset.id;
-    const attribute = X.first('#levelUpOverlay .attribute-pick.selected').dataset.attribute;
-
-    EnlightenSystem.chooseLevelUpAttribute(id, attribute);
-    WindowManager.pop();
-    resumeEssenceBar(id);
-  }
-
-  function resumeEssenceBar(id) {
+  function resume(id) {
     X.addClass(`#enlightenView .level-up-button[data-id='${id}']`,'hide');
     animateEssenceBar(id, levelBounds(id).floor);
   }
@@ -197,8 +150,9 @@ global.EnlightenView = (function() {
   }
 
   return Object.freeze({
-    init: init,
-    show: show,
+    init,
+    show,
+    resume,
   });
 
 })();
--- application/views/enlighten/level-up-overlay.js
+++ application/views/enlighten/level-up-overlay.js
@@ -1,11 +1,57 @@
 global.LevelUpOverlay = (function() {
 
-  function open(id) {
+  function init() {
+    X.onClick('#levelUpOverlay .attribute-pick', pickAttribute);
+    X.onClick('#levelUpOverlay .confirm-button', confirmLevelUp);
+  }
 
+  function open(id) {
+    GeneralOverlay.open(build(id), { classname:'small' });
+  }
+
+  function build(id) {
+    const experience = ExperienceComponent.lookup(id);
+    const attributes = AttributesComponent.lookup(id);
+
+    const content = X.createElement(`<div id='levelUpOverlay' data-id='${id}'>
+      <div class='title'>${Character(id).getName()}</div>
+      <p class='summary'>Level ${experience.level} → ${experience.level + 1}. Choose an attribute to raise.</p>
+      <ul class='attribute-picks'></ul>
+      <div class='button-row align-right'>
+        <a href='#' class='button button-primary confirm-button disabled'>Confirm</a>
+      </div>
+    </div>`);
+
+    Object.keys(Attrib).forEach(code => {
+      X.append(content.querySelector('.attribute-picks'), X.createElement(`<li>
+        <a href='#' class='attribute-pick' data-attribute='${code}'>
+          <span class='attribute-name'>${StringHelper.titlecase(code)}</span>
+          <span class='attribute-value'>${attributes[code]}</span>
+        </a>
+      </li>`));
+    });
+
+    return content;
+  }
+
+  function pickAttribute(event) {
+    X.removeClass('#levelUpOverlay .attribute-pick','selected');
+    X.addClass(event.target.closest('.attribute-pick'),'selected');
+    X.removeClass('#levelUpOverlay .confirm-button','disabled');
+  }
+
+  function confirmLevelUp() {
+    const id = X.first('#levelUpOverlay').dataset.id;
+    const attribute = X.first('#levelUpOverlay .attribute-pick.selected').dataset.attribute;
+
+    EnlightenSystem.chooseLevelUpAttribute(id, attribute);
+    WindowManager.pop();
+    EnlightenView.resume(id);
   }
 
   return Object.freeze({
-    open
+    init,
+    open,
   });
 
 })();
