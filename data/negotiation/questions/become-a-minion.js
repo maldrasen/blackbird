07e9151ec8a433db9703676f857b468812034af9
Maldrasen <maldrasen@gmail.com>
2026-08-03 15:31:13 -0500

    Add a conversation contest here

--- data/negotiation/questions/become-a-minion.js
+++ data/negotiation/questions/become-a-minion.js
@@ -12,7 +12,12 @@ NegotiationQuestion.registerReaction('become-a-minion', {
   style: NegotiationStyle.fierce,
   reactions: {
     liar: NegotiationReaction.respect(`{T:He} chuckles and shakes {T:his} head, "Heh, fine. Not yet anyway."`),
-    backwards: NegotiationReaction.dislike(`"Me mer me mi ma ma mackmards," {T:he} says mockingly, "Gods you sound like a douche."`),
+    // TODO: Placeholder win prose, rewrite as needed.
+    backwards: NegotiationReaction.contest({
+      skill: 'conversation',
+      win: NegotiationReaction.respect(`{T:He} squints at you for a long moment, then barks out a laugh, "Heh. Ballsy. Fine, maybe you're not a complete pussy."`),
+      loss: NegotiationReaction.dislike(`"Me mer me mi ma ma mackmards," {T:he} says mockingly, "Gods you sound like a douche."`),
+    }),
     takeCharge: NegotiationReaction.disrespect(`{T:He} gives you a mocking grin, "You {P:species.elves} are such pussies."`),
   }
 });
