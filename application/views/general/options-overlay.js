293e8ea94014b3958395882c1ad168e790686b0a
Maldrasen <maldrasen@gmail.com>
2026-08-04 14:14:31 -0500

    Save options

--- application/views/general/options-overlay.js
+++ application/views/general/options-overlay.js
@@ -31,6 +31,7 @@ global.OptionsOverlay = (function() {
       max: difficulty.damage.max,
       step: difficulty.damage.step,
       inputSelector: difficulty.damage.input,
+      onChange: markDirty,
     }).getElement());
 
     X.first('#healthSlider').appendChild(Slider({
@@ -39,6 +40,7 @@ global.OptionsOverlay = (function() {
       max: difficulty.health.max,
       step: difficulty.health.step,
       inputSelector: difficulty.health.input,
+      onChange: markDirty,
     }).getElement());
 
     X.first('#resistanceSlider').appendChild(Slider({
@@ -47,6 +49,7 @@ global.OptionsOverlay = (function() {
       max: difficulty.resistance.max,
       step: difficulty.resistance.step,
       inputSelector: difficulty.resistance.input,
+      onChange: markDirty,
     }).getElement());
 
     isBuilt = true;
@@ -64,33 +67,22 @@ global.OptionsOverlay = (function() {
     MainMenu.show();
   }
 
+  function markDirty() {
+    isDirty = true;
+  }
+
   function save() {
-    if (isDirty) {
-      WorldState.setOptions(pack()).then(saveSuccessful);
-    }
-  }
+    if (isDirty === false) { return; }
 
-  function saveSuccessful() {
-    Alert.show({
-      message: 'Options Saved',
-      position: AlertPosition.side,
-      type: LogType.success,
-      fadeTime: 1000,
+    WorldState.setOptions(pack()).then(() => {
+      isDirty = false;
+      Alert.show({
+        message: 'Options Saved',
+        position: AlertPosition.side,
+        type: LogType.success,
+        fadeTime: 1000,
+      });
     });
-  }
-
-  // We need to validate the input before we save these values in the world state, but we should also validate when
-  // any of the inputs are updated by hand. Can't be done on key press though. Maybe on loss of focus, but we need to
-  // ensure that focus is lost before the window is closed, or maybe if the value is set to something invalid we set
-  // the input to the default value before returning it.
-  function difficultyValue(key) {
-    const value = Number(X.first(difficulty[key].input).value);
-
-    // Wait... Not a number is a number isn't it? But it's also not null. Test for raw NaN?
-    if (typeof value != "number") { return difficulty[key].default; }
-    if (value < difficulty[key].min) { return difficulty[key].min; }
-    if (value > difficulty[key].max) { return difficulty[key].max; }
-    return value;
   }
 
   function pack() {
@@ -103,6 +95,17 @@ global.OptionsOverlay = (function() {
     };
   }
 
+  // TODO: Is this necessary? The Slider's change handler validates, but can save every be invoked before change is fired?
+  function difficultyValue(key) {
+    const value = Number(X.first(difficulty[key].input).value);
+
+    if (isNaN(value)) { return difficulty[key].default; }
+    if (value < difficulty[key].min) { return difficulty[key].min; }
+    if (value > difficulty[key].max) { return difficulty[key].max; }
+    return value;
+  }
+
+  // TODO: Are we ever using this or the one in the MainMenu?
   function toString() { return `OptionsOverlay` }
 
   return {
