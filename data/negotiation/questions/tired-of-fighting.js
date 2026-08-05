27c63229f2167b4b54942e48ad95cab3d67cccb2
Maldrasen <maldrasen@gmail.com>
2026-08-05 13:07:54 -0500

    Adding another reaction

--- data/negotiation/questions/tired-of-fighting.js
+++ data/negotiation/questions/tired-of-fighting.js
@@ -2,8 +2,17 @@
 NegotiationQuestion.register('tired-of-fighting', {
   text: `What's the matter? You tired of fighting?`,
   answers: {
-    'all-day':   { text:`"Tired? I could do this all day."` },
-    'tiresome':  { text:`"It is awfully tiresome though isn't it?"` },
-    'other-way': { text:`"Perhaps we could settle this some other way?"` },
+    allDay:    { text:`"Tired? I could do this all day."` },
+    tiresome:  { text:`"It is awfully tiresome though isn't it?"` },
+    otherWay:  { text:`"Perhaps we could settle this some other way?"` },
+  }
+});
+
+NegotiationQuestion.registerReaction('tired-of-fighting', {
+  style: NegotiationStyle.fierce,
+  reactions: {
+    allDay:   NegotiationReaction.attack(`The {T:species.elf} grins, "Good. Don't pussy out on me just when things are getting good."`),
+    tiresome: NegotiationReaction.disrespect(`"Tiresome? Oh so sorry to interrupt your godsdamned tea time."`),
+    otherWay: () => { throw new Error(`This needs to be a follow up question.`) },
   }
 });
--- docs/Scratch.md
+++ docs/Scratch.md
@@ -1,3 +1,3 @@
-
-TODO: Answer level requirements
 TODO: Only show colors in battle text
+
+OK, we might actually need join resolution next between the last reaction and before the resolution text is shown. If the resolution is attack, or run, I think it's fine to leave it with just the alert type text, but it is a little strange for a monster to join without comment when the negotiation times out... Maybe we resolve to an intrim type then then resolves to a final type?
