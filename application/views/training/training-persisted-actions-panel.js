26fd9c913fdea4b35f79e90753bce5f570305f15
Maldrasen <maldrasen@gmail.com>
2026-08-07 18:57:16 -0500

    Update persistant action panels

--- application/views/training/training-persisted-actions-panel.js
+++ application/views/training/training-persisted-actions-panel.js
@@ -1,16 +1,12 @@
 global.TrainingPersistedActionsPanel = (function() {
 
-  let persistedScrollingPanel;
-
   function init() {
     window.addEventListener('resize', calculatePersistedHeight);
     X.onClick('#persistedActions .stop-action', stopAction);
   }
 
   function build() {
-    persistedScrollingPanel = ScrollingPanel({ id:'#persistedActionScroll' });
     calculatePersistedHeight();
-
   }
 
   function update() {
@@ -69,19 +65,16 @@ global.TrainingPersistedActionsPanel = (function() {
   // force the height of the final element to force it to fill the proper space. We'll need to call this every round
   // because the heights of many of the other panels will change.
   function calculatePersistedHeight() {
-    const statusRow = X.first('#statusRow');
-    const scalesRow = X.first('#scalesRow');
-    const actionRow = X.first('#actionRow');
+    const panel = X.first('#persistedActionScroll');
+    if (panel == null) { return; }
 
-    if (persistedScrollingPanel) {
-      const status = X.getPosition(statusRow).height;
-      const scales = X.getPosition(scalesRow).height;
-      const action = X.getPosition(actionRow).height;
-      const heights = status + scales + action + 25
+    const status = X.getPosition(X.first('#statusRow')).height;
+    const scales = X.getPosition(X.first('#scalesRow')).height;
+    const action = X.getPosition(X.first('#actionRow')).height;
+    const heights = status + scales + action + 25
 
-      persistedScrollingPanel.setHeight(window.innerHeight - heights);
-      persistedScrollingPanel.resize();
-    }
+    // The inline style would override the 200px cap from the stylesheet, so it has to be applied here too.
+    panel.style.maxHeight = `${Math.min(200, window.innerHeight - heights)}px`;
   }
 
   return Object.freeze({
