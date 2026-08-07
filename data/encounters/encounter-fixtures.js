5032c0d9215d38f6bf49b8d38e359471b3c3ca2c
Maldrasen <maldrasen@gmail.com>
2026-08-06 20:52:29 -0500

    These periods were bothering me.

--- data/encounters/encounter-fixtures.js
+++ data/encounters/encounter-fixtures.js
@@ -2,25 +2,25 @@
 // Encounter fixtures. These shouldn't go into any of the encounter tables.
 
 Encounter.register('negotiation-fixture-1',{
-  description: `someone to talk to.`,
+  description: `someone to talk to`,
   formation:[[0,0,1,0,0]],
   monsters: { 1: { code:'kobold-runt' }}
 });
 
 Encounter.register('negotiation-fixture-2',{
-  description: `someone to talk to.`,
+  description: `someone to talk to`,
   formation:[[0,0,1,0,0]],
   monsters: { 1: { code:'kobold-sneak-slut' }}
 });
 
 Encounter.register('negotiation-fixture-3',{
-  description: `someone to talk to.`,
+  description: `someone to talk to`,
   formation:[[0,0,1,0,0]],
   monsters: { 1: { code:'kobold-dick-puncher' }}
 });
 
 Encounter.register('negotiation-fixture-4',{
-  description: `someone to talk to.`,
+  description: `someone to talk to`,
   formation:[[0,0,1,0,0]],
   monsters: { 1: { code:'kobold-tosser' }}
 });
