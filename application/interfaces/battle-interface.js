6df054847156e8513c3332de5f25f151068438c8
Maldrasen <maldrasen@gmail.com>
2026-08-15 17:14:07 -0500

    Update guards in the battle interface.

--- application/interfaces/battle-interface.js
+++ application/interfaces/battle-interface.js
@@ -1,59 +1,58 @@
 global.BattleInterface = (function() {
 
+  function viewActive() {
+    if (Environment.viewPresent() === false) { return false }
+    return GameSystem.getState().getGameMode() === GameMode.battle;
+  }
+
   function startTargeting(positions, allies) {
-    if (Environment.viewPresent() === false) { return; }
-    FormationPanel.startTargeting(positions, allies);
+    if (viewActive()) { FormationPanel.startTargeting(positions, allies); }
   }
 
   function showCharacterCommands() {
-    if (Environment.viewPresent() === false) { return; }
-    CommandPanel.showCommands(CharacterAbilitySystem.getAbilities());
+    if (viewActive()) { CommandPanel.showCommands(CharacterAbilitySystem.getAbilities()); }
   }
 
   function showMonsterResult() {
-    if (Environment.viewPresent() === false) { return; }
+    if (viewActive() === false) { return; }
     CommandPanel.hide();
     BattleText.setMessages(BattleSystem.getRound().getMessages());
     BattleView.update();
   }
 
   function showCharacterResult() {
-    if (Environment.viewPresent() === false) { return; }
+    if (viewActive() === false) { return; }
     CommandPanel.hide();
     BattleText.setMessages(BattleSystem.getRound().getMessages());
     BattleView.update();
   }
 
   function updateCombatantView(id) {
-    if (Environment.viewPresent() === false) { return; }
-    FormationPanel.updateCombatant(id)
+    if (viewActive()) { FormationPanel.updateCombatant(id); }
   }
 
   function highlightActing() {
-    if (Environment.viewPresent() === false) { return; }
-    FormationPanel.highlightActing(BattleSystem.getRound().getActing());
+    if (viewActive()) { FormationPanel.highlightActing(BattleSystem.getRound().getActing()); }
   }
 
   function showDamageEffect(data) {
-    if (Environment.viewPresent() === false) { return; }
-    FormationPanel.showDamageEffect(data);
+    if (viewActive()) { FormationPanel.showDamageEffect(data); }
   }
 
   function killEntity(id) {
-    if (Environment.viewPresent() === false) { return; }
-    FormationPanel.killEntity(id);
+    if (viewActive()) { FormationPanel.killEntity(id); }
   }
 
   function moveForwardOnDeath(columnData) {
-    if (Environment.viewPresent() === false) { return; }
-    FormationPanel.moveForwardOnDeath(columnData);
+    if (viewActive()) { FormationPanel.moveForwardOnDeath(columnData); }
   }
 
   function moveInwardOnDeath(moves) {
-    if (Environment.viewPresent() === false) { return; }
-    FormationPanel.moveInwardOnDeath(moves);
+    if (viewActive()) { FormationPanel.moveInwardOnDeath(moves); }
   }
 
+  // Not a call into the battle view. This starts an episode and changes the mode, so it's gated on the client being
+  // present rather than on the battle view being active.
   function showGameOver() {
     if (Environment.viewPresent() === false) { return; }
     EpisodeSystem.startEpisode('game-over', {});
