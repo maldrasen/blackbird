f121967ebc4b56260ef0804fde401c36449710cb
Maldrasen <maldrasen@gmail.com>
2026-08-17 19:21:53 -0500

    Turn the essence script into a data generator

--- bin/build.sh
+++ bin/build.sh
@@ -4,5 +4,8 @@
 cd $BLACKBIRD_HOME
 rm -rf ./dist/*
 
+# Regenerate precalculated data
+node ./bin/compile-essence-data.js
+
 # Electron Builder
 npm run dist
---
+++ bin/compile-essence-data.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+exec node ./bin/compile-essence-data.js
