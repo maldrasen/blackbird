c0f276898e10993cf401262b0f899033134ddc19
Maldrasen <maldrasen@gmail.com>
2026-08-21 10:21:33 -0500

    Add new battle predicates

--- application/requirements/battle-requirements.js
+++ application/requirements/battle-requirements.js
@@ -1,6 +1,14 @@
 global.BattleRequirements = (function() {
 
+  function activeMonsterCount() {
+    return BattleSystem.getState().getActiveMonsters().length;
+  }
+
   return {
+    againstSingle: () => { return (context) => { return activeMonsterCount() === 1; }},
+    againstMultiple: () => { return (context) => { return activeMonsterCount() > 1; }},
+    againstAtLeast: x => { return (context) => { return activeMonsterCount() >= x }},
+    againstAtMost: x => { return (context) => { return activeMonsterCount() <= x }},
     withHitLocation: (...slots) => { return (context) => { return slots.includes(context.hitLocation); }},
   };
 
