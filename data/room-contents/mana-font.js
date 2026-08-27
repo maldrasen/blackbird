8f9c9cf3de6109bfa61307add56909b2330d3651
Maldrasen <maldrasen@gmail.com>
2026-08-27 17:24:58 -0400

    Cases aren't object keys

--- data/room-contents/mana-font.js
+++ data/room-contents/mana-font.js
@@ -7,11 +7,11 @@ function describe(options) {
 
 function describeStatue(color) {
   switch (color) {
-    case [Mana.red]: return `roaring and shamelessly erect dragon, it's huge ridged cock thrust forward aggressively.`;
-    case [Mana.yellow]: return `grinning havlin, holding a fist sized gemstone in one hand and a hammer in the other.`;
-    case [Mana.green]: return `plush nymph, cupping and squeezing her large breasts, pushing her long hard nipples outward.`;
-    case [Mana.blue]: return `regal looking futanari elf with a sharp and cruel smile, hir robes parted just enough to tease.`;
-    case [Mana.black]: return `athletic looking sylph, balanced on one foot while holding her other leg behind her head.`;
+    case Mana.red:    return `roaring and shamelessly erect dragon, it's huge ridged cock thrust forward aggressively.`;
+    case Mana.yellow: return `grinning havlin, holding a fist sized gemstone in one hand and a hammer in the other.`;
+    case Mana.green:  return `plush nymph, cupping and squeezing her large breasts, pushing her long hard nipples outward.`;
+    case Mana.blue:   return `regal looking futanari elf with a sharp and cruel smile, hir robes parted just enough to tease.`;
+    case Mana.black:  return `athletic looking sylph, balanced on one foot while holding her other leg behind her head.`;
   }
 }
 
