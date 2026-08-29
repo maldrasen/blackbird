818298720229c750c1f589ec8b930b10af040621
Maldrasen <maldrasen@gmail.com>
2026-08-29 13:13:41 -0500

    Update descriptions

--- data/room-contents/orchards.js
+++ data/room-contents/orchards.js
@@ -9,15 +9,14 @@ function getSizeCategory() {
 function describe() {
   const category = getSizeCategory();
 
-  if (category === 'small') {
-    return `A Small Orchard`;
-  }
+  if (category === 'small') { return `The twisted trees in this small underground orchard are tall enough to brush
+    against the chamber's stone ceiling.`; }
 
-  if (category === 'large') {
-    return `A Large Orchard`;
-  }
+  if (category === 'large') { return `The long rows of trees in this large underground orchard stretch off into the 
+    darkness, providing both food for the dungeon's denizens, as well as ample places to ambush from.`; }
 
-  return `A Modest Orchard`;
+  return `The underground orchard is strangely dark and still. No breeze stirs the leaves of these trees, and no 
+    sunlight ever shines upon them.`;
 }
 
 RoomContents.register('orchard-empty',{
