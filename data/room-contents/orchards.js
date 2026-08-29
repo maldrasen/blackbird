e28c3b4c3871f26181347457464eca44d390f038
Maldrasen <maldrasen@gmail.com>
2026-08-29 12:41:25 -0500

    Todo, description based on size

--- data/room-contents/orchards.js
+++ data/room-contents/orchards.js
@@ -1,7 +1,23 @@
 
-function describe() {
+function getSizeCategory() {
   const size = DungeonSystem.getDungeonFloor().getCurrentRoom().getContentsOptions().size;
-  return `TODO: An orchard with size ${size}`;
+  if (size > 25) { return 'large'; }
+  if (size < 18) { return 'small'; }
+  return 'modest'
+}
+
+function describe() {
+  const category = getSizeCategory();
+
+  if (category === 'small') {
+    return `A Small Orchard`;
+  }
+
+  if (category === 'large') {
+    return `A Large Orchard`;
+  }
+
+  return `A Modest Orchard`;
 }
 
 RoomContents.register('orchard-empty',{
