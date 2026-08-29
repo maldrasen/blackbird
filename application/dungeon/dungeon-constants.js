2982f79d041cdfb7abb99116703cdf8cc03e2291
Maldrasen <maldrasen@gmail.com>
2026-08-29 11:00:39 -0500

    Add an orchard feature

--- application/dungeon/dungeon-constants.js
+++ application/dungeon/dungeon-constants.js
@@ -2,4 +2,17 @@ global.DungeonConstants = {
   fontChance: 20,
   floorTypes: ['default','water'],
   wallColor: 'rgb(130 130 140)',
+
+  treeColors:[
+    'rgb(51,62,45)',
+    'rgb(65,101,73)',
+    'rgb(54,60,49)',
+    'rgb(87,99,77)',
+    'rgb(38,60,43)',
+    'rgb(45,62,48)',
+    'rgb(68,85,61)',
+    'rgb(80,89,69)',
+    'rgb(40,60,44)',
+    'rgb(46,53,42)',
+  ],
 };
---
+++ data/feature-types/orchard.js
@@ -0,0 +1,37 @@
+FeatureType.register('orchard', {
+  build: function(options) {
+    const feature = Feature('orchard');
+    const room = Room(feature);
+    const width = Random.between(options.size[0], options.size[1]);
+    const height = Random.between(options.size[0], options.size[1]);
+
+    room.setBounds(width, height);
+    room.addBox(0, 0, width, height);
+    room.allowStairs();
+
+    Random.flipCoin() ? addTreeRows(room, width, height) : addTreeCols(room, width, height);
+
+    feature.addRoom(room);
+    return feature;
+  }
+});
+
+function addTreeRows(room, width, height) {
+  for (let yy=0; yy < height; yy++) {
+    for (let xx=0; xx < (width*2)-1; xx++) {
+      const x = 0.5 + xx/2;
+      const y = 0.5 + yy;
+      room.addGlyph({ x:x, y:y, glyph:'✽', color:Random.from(DungeonConstants.treeColors), size:Random.between(48,56) });
+    }
+  }
+}
+
+function addTreeCols(room, width, height) {
+  for (let xx=0; xx < width; xx++) {
+    for (let yy=0; yy < (height*2)-1; yy++) {
+      const x = 0.5 + xx;
+      const y = 0.5 + yy/2;
+      room.addGlyph({ x:x, y:y, glyph:'✽', color:Random.from(DungeonConstants.treeColors), size:Random.between(48,56) });
+    }
+  }
+}
