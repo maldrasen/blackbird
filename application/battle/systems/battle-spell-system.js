39e077c2d66965338c409321075924c64e2fdc33
Maldrasen <maldrasen@gmail.com>
2026-08-31 16:08:43 -0500

    Add a skill check to casting a spell

--- application/battle/systems/battle-spell-system.js
+++ application/battle/systems/battle-spell-system.js
@@ -2,11 +2,18 @@ global.BattleSpellSystem = (function() {
 
   // The spell's casting time was already spent when the cast began, so the release itself only takes a moment.
   function castSpell() {
+    const round = BattleSystem.getRound();
+    const acting = round.getActing();
     const spellData = BattleSystem.getState().finishCastingSpell();
     const spell = Spell.lookup(spellData.code);
+    const check = spell.rollSkillCheck(acting, spellData.powerLevel);
 
-    BattleSystem.getRound().addTime(500);
-    EffectSystem.applyDuringBattle(spell, spellData.powerLevel);
+    round.addTime(500);
+    if (check.result === 'fail') {
+      return round.addMessage({ text:`{A:ActingName} fucked up casting {A:his} spell.` });
+    }
+
+    EffectSystem.applyDuringBattle(spell, check.powerLevel);
   }
 
   return {
--- application/records/spell.js
+++ application/records/spell.js
@@ -23,17 +23,66 @@ global.Spell = (function() {
       return (powerLevel * multiplier) + base;
     }
 
+    function getManaCost(powerLevel=1) {
+      return spell.manaCost * powerLevel;
+    }
+
+    function getSpellSkill() {
+      switch (spell.color) {
+        case Mana.red: return 'sorcery';
+        case Mana.yellow: return 'thaumaturgy';
+        case Mana.green: return 'witchcraft';
+        case Mana.blue: return 'magic';
+        case Mana.black: return 'wizardry';
+      }
+    }
+
+    // When a spell is cast we roll the associated magic skill against the spell's difficulty level, which (for now at
+    // least) is the amount of mana used to cast the spell. If the skill check was a crit, then the spell's power level
+    // is increased.
+    function rollSkillCheck(caster, powerLevel) {
+      const check = SkillCheck(caster, getSpellSkill());
+      const difficulty = getManaCost(powerLevel);
+
+      if (check.fumble || check.value < difficulty) {
+        return { result:'fail' }
+      }
+
+      return {
+        result: 'success',
+        powerLevel: check.crit ? boost(powerLevel) : powerLevel
+      };
+    }
+
+    // For each level boost() increases resulting level by 2, 3, 4, or 5. A critical boost is more significant at
+    // lower levels, tripling then doubling damage, tapering off to around a 150% boost at higher levels, but still
+    // remains significant. Assuming the highest upcast is level 8, resulting in a max power level of 13x.
+    function boost(level) {
+      switch (level) {
+        case 1: return 3;
+        case 2: return 4;
+        case 3: return 6;
+        case 4: return 7;
+        case 5: return 9;
+        case 6: return 10;
+        case 7: return 12;
+        case 8: return 13;
+      }
+    }
+
     return {
       getCode: () => { return code; },
       getName: () => { return spell.name; },
       getColor: () => { return spell.color; },
-      getManaCost: (powerLevel=1) => { return spell.manaCost * powerLevel; },
+      getManaCost,
       getCastingTime,
       getTarget: () => { return spell.target || EffectTarget.self; },
       getAreaOfEffect: () => { return spell.areaOfEffect; },
       getEffects: (powerLevel=1) => { return spell.getEffects(powerLevel); },
       pickStory: context => { return spell.stories.pick(context); },
       messageForEntity: (id,results) => { return spell.messageForEntity(id,results); },
+      getSpellSkill,
+      rollSkillCheck,
     };
   }
 
