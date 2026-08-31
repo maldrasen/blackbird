f031eced44c1f98f4ed374882e27e962ed5e240f
Maldrasen <maldrasen@gmail.com>
2026-08-31 12:51:48 -0500

    Add a casting ability

--- application/test/fixtures.js
+++ application/test/fixtures.js
@@ -11,7 +11,8 @@ global.Fixtures = (function() {
     BattleFixtures.prepareForBattle();
     BattleSystem.startBattle({
       afterBattle: 'returnTo.mainMenu',
-      monster: 'kobold-tosser',
+      monster: 'flamescale-screamer',
+      // monster: 'kobold-tosser',
       // encounter: 'orchard-kobolds',
     });
 
--- data/monsters/flamescale-screamer.js
+++ data/monsters/flamescale-screamer.js
@@ -5,4 +5,9 @@ BaseMonster.register('flamescale-screamer',{
   archetypes: { maniac:10 },
   triggers: ['red-hair'],
   level: 8,
+
+  prioritizedAbilities:[
+    { code:'monster-cast-spell', priority:80, spell:'overwhelming-effulgence', powerLevel:2, cooldown:2000 },
+  ],
+
 });
