c4d1bcc903028ea1f335acc7b5b3b900e7baa9ce
Maldrasen <maldrasen@gmail.com>
2026-08-31 09:32:55 -0500

    Come back to this later

--- test/characters/describers/cock-describer-spec.js
+++ test/characters/describers/cock-describer-spec.js
@@ -1,44 +1,32 @@
 describe('CockDescriber', function() {
   describe("buildFullDescription()", function() {
 
-    it.only("describes humans with tiny cocks", function() {
+    // TODO: Not much that can be tested with these. We should at least test the description for weaver error and
+    //       warning messages. Mostly this is a "print the description to the console" kind of spec.
+
+    it("describes humans with tiny cocks", function() {
       const dude = CharacterFactory.build({ species:'human', gender:'male', cock:{ size:'tiny' }});
       const description = CockDescriber(dude).buildFullDescription();
-
-      console.log("=== Description ===")
-      console.log(description);
     });
 
     it("describes humans with small cocks", function() {
       const dude = CharacterFactory.build({ species:'human', gender:'male', cock:{ size:'small' }});
       const description = CockDescriber(dude).buildFullDescription();
-
-      console.log("=== Description ===")
-      console.log(description);
     });
 
     it("describes humans with average cocks", function() {
       const dude = CharacterFactory.build({ species:'human', gender:'male', cock:{ size:'average' }});
       const description = CockDescriber(dude).buildFullDescription();
-
-      console.log("=== Description ===")
-      console.log(description);
     });
 
     it("describes humans with big cocks", function() {
       const dude = CharacterFactory.build({ species:'human', gender:'male', cock:{ size:'big' }});
       const description = CockDescriber(dude).buildFullDescription();
-
-      console.log("=== Description ===")
-      console.log(description);
     });
 
     it("describes humans with huge cocks", function() {
       const dude = CharacterFactory.build({ species:'human', gender:'male', cock:{ size:'huge' }});
       const description = CockDescriber(dude).buildFullDescription();
-
-      console.log("=== Description ===")
-      console.log(description);
     });
 
   });
