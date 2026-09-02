8e2f0eb3b88cc4438c67d3dc680541de70150d74
Maldrasen <maldrasen@gmail.com>
2026-09-02 09:01:55 -0500

    Update entrance description

--- data/episodes/dungeon/dungeon-entrance.js
+++ data/episodes/dungeon/dungeon-entrance.js
@@ -1,7 +1,20 @@
 
+const page1 = `You take a moment to thoroughly search the entrance chamber. The sound of the waterfalls outside is
+  muffled by the thick stone walls, and only a trickle of the outside light can still be seen, reflecting off of the
+  rippling water. The shallow pool is only a few inches deep, fed by several cascades, positioned behind the statues.`;
+const page2 = `The statues themselves are imposing. They're each perhaps twelve feet tall or so and exquisitely carved
+  from some dark stone. They're each unique, but the creatures all share a similar pose; standing tall, arms folded
+  over their muscular chests, their featureless faces turned down as though judging those who enter. Though none of the
+  statues are erect, they're positioned so that their thick bulging members are right at eye level. The effect is less
+  erotic than it is... threatening.`;
+const page3 = `Other than the pool and the statues though, there doesn't seem to be anything else of note in the
+  large entrance chamber.`;
+
 Episode.register('dungeon-entrance',{
   layout: 'centered',
   pages: [
-    { content:`TODO: Larger Entrance Description` }
+    { content:page1 },
+    { content:page2 },
+    { content:page3 },
   ],
 });
--- data/room-contents/dungeon-entrance.js
+++ data/room-contents/dungeon-entrance.js
@@ -1,9 +1,8 @@
 
-// Starting an episode in the dungeon entrance isn't possible without some special handling. Instead, I'm adding an
-// inspect button to optionally start this episode.
 RoomContents.register('dungeon-entrance', {
-  description: `The entrance chamber is filled with the sound of gently falling water. The rippling pool sparkles 
-    brightly, as though catching rays of sunlight. The narrow causeway, flanked by statues of twelve faceless and 
-    impossibly well endowed creatures, leads to a stout wooden door and the dungeon beyond.`,
+  description: `The entrance chamber is filled with the sound of gently falling water. The rippling pool sparkles
+    brightly, as though catching rays of sunlight, though there's no source of this light that you can see. The narrow
+    causeway, flanked by statues of twelve faceless and impossibly well endowed creatures, leads to a stout wooden door
+    and the dungeon beyond.`,
   commands:[{ code:'inspect', label:'Inspect', startEpisode:'dungeon-entrance' }],
 });
