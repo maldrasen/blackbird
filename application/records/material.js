ba410df4bc76d53601fe2275ac84683b8db894ab
Maldrasen <maldrasen@gmail.com>
2026-09-17 15:01:23 -0500

    Merging equipment

--- application/records/material.js
+++ application/records/material.js
@@ -17,6 +17,7 @@ global.Material = (function() {
   function forType(type) {
     switch (type) {
       case MaterialType.leather: return ['leather'];                   // Some items, like whips can only really be made with leather.
+      case MaterialType.wood:    return ['wood'];                      // Wooden shields.
       case MaterialType.bendy:   return ['wood','steel'];              // Bows and crossbows.
       case MaterialType.pliable: return ['wool','silk','leather'];     // Clothing.
       case MaterialType.hard:    return [...metals];                   // Rigid metal armors and equipment that should only be metal.
