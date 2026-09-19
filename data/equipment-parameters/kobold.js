6f9f81b50c0434fe81561fe21b225ee5b9be1e47
Maldrasen <maldrasen@gmail.com>
2026-09-19 13:02:30 -0500

    Update kobold and vermen depots.

--- data/equipment-parameters/kobold.js
+++ data/equipment-parameters/kobold.js
@@ -9,7 +9,34 @@ EquipmentParameters.register('kobold',{
     iron: 30,
   },
 
-  equipment:{},
+  equipment:{
+    'hatchet':           40,
+    'cleaver':           30,
+    'hand-axe':          20,
+    'shortbow':          50,
+    'knife':             50,
+    'dagger':            30,
+    'hammer':            30,
+    'mace':              20,
+    'warhammer':         10,
+    'spear':             80,
+    'leather-buckler':   30,
+    'wood-buckler':      20,
+    'leather-targe':     30,
+    'wood-targe':        20,
+    'short-sword':       50,
+    // Weapon Total      510
+
+    // Armor - No chests or helms yet. Ratio should be 2 weapons for 3 armors. I should add some more crude armors,
+    //         leather straps and such.
+    'boots':             220,
+    'gloves':            150,
+    'bracers':           120,
+    'leggings':          140,
+    'chaps':             120,
+    // Armor Total       750
+  },
+
   enchantments:{},
 
 });
--- data/equipment-parameters/vermen.js
+++ data/equipment-parameters/vermen.js
@@ -9,7 +9,30 @@ EquipmentParameters.register('vermen',{
     iron: 20,
   },
 
-  equipment:{},
-  enchantments:{},
+  // Vermen don't have any armor in their depot yet. This is in part to make sure that even if a depot completely runs
+  // dry in one type of equipment or another they'll still work. I kind of like this from a world building perspective
+  // as well. To a vermen, life is so cheap that armor is considered worthless, cowardly even. Better to always be
+  // naked and screaming.
+  equipment:{
+    'hatchet':           15,
+    'cleaver':           15,
+    'hand-axe':          10,
+    'shortbow':          20,
+    'knife':             50,
+    'dagger':            40,
+    'spear':             30,
+    'scythe':            25,
+    'short-sword':       20,
+    'bullwhip':          25,
+    'chain-whip':        10,
+    'sickle-and-chain':  25,
+    'ball-and-chain':    15,
+    'flail':             25,
+    'cat-o-nine-tails':  15,
+  },
+
+  enchantments:{
+    // Poison, venom and diseased weapons should be common.
+  },
 
 });
