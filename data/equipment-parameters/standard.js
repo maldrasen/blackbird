467a01c4c8af2021dcd89280dfd574a211075501
Maldrasen <maldrasen@gmail.com>
2026-09-19 12:48:21 -0500

    Add all current equipment to standard depot parameters.

--- data/equipment-parameters/standard.js
+++ data/equipment-parameters/standard.js
@@ -10,36 +10,122 @@ EquipmentParameters.register('standard',{
     silver: 10,
   },
 
-  // Armor and weapons are combined in the same map as they're all equipment. Armor should be 6 times more common
-  // than weapons. A character equipping themselves will get 1 or 2 weapons, but get 5-7 armor pieces.
+  // Armor and weapons are combined in the same map as they're all equipment. Armor should be 6 times more common than
+  // weapons. A character equipping themselves will get 1 or 2 weapons, but get 5-7 armor pieces. Because shields take
+  // a hand slot they're counted on the weapon side.
   equipment:{
+    'hatchet':           30,
+    'cleaver':           35,
+    'hand-axe':          40,
+    'broad-axe':         50,
+    'war-axe':           50,
+    'goosewing':         5,
+    'battle-axe':        15,
+    'labrys':            5,
+    // Axes:             230
 
-    'hatchet':       30,
-    'cleaver':       35,
-    'hand-axe':      40,
-    'broad-axe':     50,
-    'war-axe':       50,
-    'goosewing':     5,
-    'battle-axe':    15,
-    'labrys':        5,
-    // Axes:         230
+    'shortbow':          40,
+    'longbow':           60,
+    'recursive-bow':     40,
+    'crossbow':          30,
+    'arbalest':          20,
+    // Bows:             190
 
-    'shortbow':      40,
-    'longbow':       60,
-    'recursive-bow': 40,
-    'crossbow':      30,
-    'arbalest':      20,
-    // Bows:         190
+    'knife':             50,
+    'dirk':              40,
+    'dagger':            50,
+    'poignard':          25,
+    'baselard':          20,
+    'stiletto':          15,
+    // Daggers:          200
 
-    'doublet':       600,
-    'hauberk':       500,
-    'cuirass':       400,
-    'breastplate':   300,
-    'plate':         100,
-    // Chests:       1900,
+    'hammer':            35,
+    'mace':              60,
+    'warhammer':         40,
+    'morning-star':      30,
+    'maul':              15,
+    // Maces:            180
 
+    'spear':             70,
+    'pike':              30,
+    'halberd':           30,
+    'glaive':            20,
+    'scythe':            15,
+    'trident':           15,
+    'ranseur':           20,
+    // Polearms:         200
+
+    'leather-buckler':   20,
+    'wood-buckler':      25,
+    'buckler':           15,
+    'leather-targe':     15,
+    'wood-targe':        20,
+    'targe':             15,
+    'wood-round-shield': 30,
+    'round-shield':      20,
+    'wood-kite-shield':  15,
+    'kite-shield':       10,
+    'heater-shield':     10,
+    'tower-shield':      5,
+    // Shields:          200
+
+    'short-sword':       50,
+    'cutlass':           30,
+    'saber':             25,
+    'scimitar':          25,
+    'rapier':            15,
+    'estoc':             10,
+    'longsword':         50,
+    'broadsword':        35,
+    'falchion':          25,
+    'bastard-sword':     20,
+    'claymore':          15,
+    // Swords:           300
+
+    'bullwhip':          20,
+    'chain-whip':        10,
+    'sickle-and-chain':  5,
+    'ball-and-chain':    10,
+    'flail':             25,
+    'cat-o-nine-tails':  10,
+    // Whips:            80
+
+    'doublet':           600,
+    'hauberk':           500,
+    'cuirass':           400,
+    'breastplate':       300,
+    'plate':             100,
+    // Chests:           1900
+
+    'boots':             800,
+    'buskins':           450,
+    'sabatons':          350,
+    'sollerets':         300,
+    // Feet:             1900
+
+    'gloves':            700,
+    'bracers':           500,
+    'vambraces':         400,
+    'gauntlets':         300,
+    // Hands:            1900
+
+    'coif':              500,
+    'helm':              550,
+    'bascinet':          350,
+    'sallet':            250,
+    'armet':             150,
+    'heaume':            100,
+    // Head:             1900
+
+    'leggings':          600,
+    'chaps':             450,
+    'greaves':           400,
+    'chainmail':         300,
+    'plate-mail':        150,
+    // Legs:             1900
   },
+  // WEAPONS TOTAL:    1580
+  // ARMOR TOTAL:      9500
 
   enchantments:{},
-
 });
