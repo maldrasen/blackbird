a0c773ddbfe635af3151cd8d03cb5e8b3f3f08bf
Maldrasen <maldrasen@gmail.com>
2026-09-19 10:55:02 -0500

    Start filling some of the equipment map in

--- data/equipment-parameters/standard.js
+++ data/equipment-parameters/standard.js
@@ -13,7 +13,36 @@ EquipmentParameters.register('standard',{
     silver: 10,
   },
 
-  equipment:{},
+  // Armor and weapons are combined in the same map as they're all equipment. Armor should be 6 times more common
+  // than weapons. A character equipping themselves will get 1 or 2 weapons, but get 5-7 armor pieces.
+  equipment:{
+
+    'hatchet':       30,
+    'cleaver':       35,
+    'hand-axe':      40,
+    'broad-axe':     50,
+    'war-axe':       50,
+    'goosewing':     5,
+    'battle-axe':    15,
+    'labrys':        5,
+    // Axes:         230
+
+    'shortbow':      40,
+    'longbow':       60,
+    'recursive-bow': 40,
+    'crossbow':      30,
+    'arbalest':      20,
+    // Bows:         190
+
+    'doublet':       600,
+    'hauberk':       500,
+    'cuirass':       400,
+    'breastplate':   300,
+    'plate':         100,
+    // Chests:       1900,
+
+  },
+
   enchantments:{},
 
 });
